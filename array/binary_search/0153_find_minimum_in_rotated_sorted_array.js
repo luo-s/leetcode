c5b9bcf9c1009f3d5d2b7d4c342fce3cf03d9da9
Shu <luo.s@outlook.com>
2023-05-01 18:35:02 -0800

    improved copy

--- array/binary_search/0153_find_minimum_in_rotated_sorted_array.js
+++ array/binary_search/0153_find_minimum_in_rotated_sorted_array.js
@@ -13,20 +13,22 @@ element of this array.
 You must write an algorithm that runs in O(log n) time.
 
 */
+// better version
 var findMin = function (nums) {
   let left = 0;
   let right = nums.length - 1;
   while (left < right) {
-    const pivot = left + Math.floor((right - left) / 2);
-    if (nums[pivot] < nums[right]) {
-      right = pivot;
+    const mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] < nums[right]) {
+      right = mid;
     } else {
-      left = pivot + 1;
+      left = mid + 1;
     }
   }
-  return nums[low];
+  return nums[left];
 };
 
+// first solution
 var findMin = function (nums) {
   // handle edge cases: empty array & single element array
   if (nums.length <= 1) {
