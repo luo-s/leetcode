ffec2b69bba49937c1274be195fbccde5d332230
Shu <luo.s@outlook.com>
2023-05-01 19:40:51 -0800

    good copy

--- array/binary_search/0035_search_insert_position.js
+++ array/binary_search/0035_search_insert_position.js
@@ -9,30 +9,15 @@ You must write an algorithm with O(log n) runtime complexity.
 var searchInsert = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
-
   while (left <= right) {
     let mid = Math.floor((left + right) / 2);
-
     if (nums[mid] < target) {
-      if (mid === nums.length - 1) {
-        return nums.length;
-      } else if (nums[mid + 1] >= target) {
-        return mid + 1;
-      } else {
-        left = mid + 1;
-      }
-    } else if (nums[mid] > target) {
-      if (mid === 0) {
-        return 0;
-      } else if (nums[mid - 1] < target) {
-        return mid;
-      } else {
-        right = mid - 1;
-      }
+      left = mid + 1;
     } else {
-      return mid;
+      right = mid - 1;
     }
   }
+  return left;
 };
 
 console.log(searchInsert([1, 3, 5, 6], 5)); // 2
