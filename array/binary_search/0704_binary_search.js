43fcaed3ee19074e17c8246b9bb96f16526701af
Shu <luo.s@outlook.com>
2023-05-03 18:32:45 -0800

    good copy

--- array/binary_search/0704_binary_search.js
+++ array/binary_search/0704_binary_search.js
@@ -3,6 +3,12 @@ Given an array of integers nums which is sorted in ascending order,
 and an integer target, write a function to search target in nums. 
 If target exists, then return its index. Otherwise, return -1.
 
+Constraints:
+1 <= nums.length <= 104
+-104 < nums[i], target < 104
+All the integers in nums are unique.
+nums is sorted in ascending order.
+
 You must write an algorithm with O(log n) runtime complexity.
 */
 
