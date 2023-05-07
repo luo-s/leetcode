cee9b4a02e9717e75145c9a651a0ef324bb9ef4c
Shu <luo.s@outlook.com>
2023-05-06 19:26:16 -0800

    good copy

---
+++ hash_table/0001_two_sum.js
@@ -0,0 +1,23 @@
+/*
+Given an array of integers nums and an integer target, return indices of the 
+two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may 
+not use the same element twice.
+
+Only one valid answer exists.
+You can return the answer in any order.
+*/
+
+// hash table
+var twoSum = function (nums, target) {
+  let checked = {};
+  for (let i = 0; i < nums.length; i++) {
+    let number = target - nums[i];
+    if (checked[number] !== undefined) {
+      return [i, checked[number]];
+    } else {
+      checked[nums[i]] = i;
+    }
+  }
+};
