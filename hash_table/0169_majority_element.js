1b320717719872c42e3f2697c35037df14df6a80
Shu <luo.s@outlook.com>
2023-05-09 08:01:51 -0800

    add boyer-moore voting algorithm

--- hash_table/0169_majority_element.js
+++ hash_table/0169_majority_element.js
@@ -26,5 +26,48 @@ var majorityElement = function (nums) {
   return majority;
 };
 
-//
-var majorityElement = function (nums) {};
+// the majority element is unique, after sorting nums[mid] return it;
+// time complexity O(nlogn)
+// space complexity O(n)
+var majorityElement = function (nums) {
+  var quickSort = function (nums) {
+    if (nums.length <= 1) {
+      return nums;
+    } else {
+      let pivot = nums[0];
+      let left = [];
+      let right = [];
+      for (let i = 1; i < nums.length; i++) {
+        if (nums[i] < pivot) {
+          left.push(nums[i]);
+        } else {
+          right.push(nums[i]);
+        }
+      }
+      return quickSort(left).concat(pivot).concat(quickSort(right));
+    }
+  };
+  return quickSort(nums)[Math.floor(nums.length / 2)];
+};
+
+// Boyer-Moore voting algorithm
+// time complexity O(n)
+// space complexity O(1)
+var majorityElement = function (nums) {
+  let candidate = nums[0];
+  let count = 1;
+  for (let i = 1; i < nums.length; i++) {
+    if (count === 0) {
+      candidate = nums[i];
+    }
+    if (nums[i] === candidate) {
+      count++;
+    } else {
+      count--;
+    }
+  }
+  return candidate;
+};
+
+console.log(majorityElement([1, 2, 1, 2, 1]));
+console.log(majorityElement([1, 2, 1, 3, 1, 3, 3, 3, 3]));
