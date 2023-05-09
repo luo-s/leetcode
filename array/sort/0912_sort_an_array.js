6e507ddc6c853433cc668eca32cd4af90743ffea
Shu <luo.s@outlook.com>
2023-05-09 09:13:28 -0800

    good copy

---
+++ array/sort/0912_sort_an_array.js
@@ -0,0 +1,51 @@
+/*
+Given an array of integers nums, sort the array in ascending order and return it.
+
+You must solve the problem without using any built-in functions in O(nlog(n)) 
+time complexity and with the smallest space complexity possible.
+*/
+
+// quick sort -- when nums is in order, time complexity is O(n^2);
+// time complexity O(nlogn)
+// space complexity O(n)
+var sortArray = function (nums) {
+  if (nums.length <= 1) {
+    return nums;
+  } else {
+    let pivot = nums[0];
+    let left = [];
+    let right = [];
+    for (let i = 1; i < nums.length; i++) {
+      if (nums[i] < pivot) {
+        left.push(nums[i]);
+      } else {
+        right.push(nums[i]);
+      }
+    }
+    return sortArray(left).concat(pivot).concat(sortArray(right));
+  }
+};
+
+// merge sort
+// time complexity O(nlogn)
+// space complexity O(n)
+var sortArray = function (nums) {
+  var merge = function (left, right) {
+    const result = [];
+    while (left.length && right.length) {
+      if (left[0] < right[0]) {
+        result.push(left.shift());
+      } else {
+        result.push(right.shift());
+      }
+    }
+    return result.concat(left).concat(right);
+  };
+  if (nums.length <= 1) {
+    return nums;
+  }
+  let mid = Math.floor(nums.length / 2);
+  let left = nums.slice(0, mid);
+  let right = nums.slice(mid);
+  return merge(sortArray(left), sortArray(right));
+};
