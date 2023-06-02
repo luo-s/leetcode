520e9268c9a65464c0abfa2c5c06883d65b9e18b
Shu <luo.s@outlook.com>
2023-06-02 09:27:38 -0800

    add solution Set

---
+++ string/basics/0242_valid_anagram.js
@@ -0,0 +1,37 @@
+/*
+Given two strings s and t, return true if t is an anagram of s, and false 
+otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different 
+word or phrase, typically using all the original letters exactly once.
+*/
+// https://leetcode.com/problems/valid-anagram/
+
+// hash table
+// time complexity O(n)
+// space complexity O(n)
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  let trackerS = {};
+  let trackerT = {};
+  for (let letter of s) {
+    if (trackerS[letter] !== undefined) {
+      trackerS[letter]++;
+    } else {
+      trackerS[letter] = 1;
+    }
+  }
+  for (let letter of t) {
+    if (trackerT[letter] !== undefined) {
+      trackerT[letter]++;
+    } else {
+      trackerT[letter] = 1;
+    }
+  }
+  if (Object.keys(trackerS).length !== Object.keys(trackerT).length)
+    return false;
+  for (let key of Object.keys(trackerS)) {
+    if (trackerS[key] !== trackerT[key]) return false;
+  }
+  return true;
+};
