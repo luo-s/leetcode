5005d3719bfb20026269ac5ef6c3f218a3f8b268
Shu <luo.s@outlook.com>
2023-06-05 12:13:32 -0800

    good copy

--- array/sort/0283_move_zeros.js
+++ array/sort/0283_move_zeros.js
@@ -4,6 +4,7 @@ the relative order of the non-zero elements.
 
 Note that you must do this in-place without making a copy of the array.
 */
+// https://leetcode.com/problems/move-zeroes/
 
 //bubble sort; two pointers
 var moveZeroes = function (nums) {
