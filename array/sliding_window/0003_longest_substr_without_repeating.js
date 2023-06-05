0f444ee1dc3505adb171a80632e22323b727f494
Shu <luo.s@outlook.com>
2023-06-05 05:59:18 -0800

    good copy

--- array/sliding_window/0003_longest_substr_without_repeating.js
+++ array/sliding_window/0003_longest_substr_without_repeating.js
@@ -4,39 +4,40 @@ characters.
 */
 // https://leetcode.com/problems/longest-substring-without-repeating-characters/
 
-// // brute force
-// // time complexity O(n^2)
-// // space complexity O(n^2)
-// var lengthOfLongestSubstring = function (s) {
-//   function isDuplicate(str) {
-//     let tracker = {};
-//     for (let letter of str) {
-//       if (tracker[letter] !== undefined) {
-//         return true;
-//       } else {
-//         tracker[letter]++;
-//       }
-//     }
-//     return false;
-//   }
-//   let result = 0;
-//   for (let i = 0; i < s.length; i++) {
-//     for (let j = i + 1; j <= s.length; j++) {
-//       let substr = s.slice(i, j);
-//       if (!isDuplicate(substr)) result = Math.max(result, substr.length);
-//     }
-//   }
-//   return result;
-// };
+// brute force
+// time complexity O(n^2)
+// space complexity O(n^2)
+var lengthOfLongestSubstring = function (s) {
+  function isDuplicate(str) {
+    let tracker = {};
+    for (let letter of str) {
+      if (tracker[letter] !== undefined) {
+        return true;
+      } else {
+        tracker[letter]++;
+      }
+    }
+    return false;
+  }
+  let result = 0;
+  for (let i = 0; i < s.length; i++) {
+    for (let j = i + 1; j <= s.length; j++) {
+      let substr = s.slice(i, j);
+      if (!isDuplicate(substr)) result = Math.max(result, substr.length);
+    }
+  }
+  return result;
+};
 
 // sliding window
+// time complexity O(n)
+// space complexity O(n)
 var lengthOfLongestSubstring = function (s) {
   if (s.length <= 1) return s.length;
   let result = 0;
   let left = 0;
   let right = 0;
   let tracker = {};
-
   while (right < s.length) {
     if (tracker[s[right]] !== undefined && left <= tracker[s[right]]) {
       left = tracker[s[right]] + 1;
