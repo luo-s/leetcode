b2c506afc2dcb9f10c9be9689b34b0a22ebc0e5b
Shu <luo.s@outlook.com>
2023-06-20 08:14:13 -0800

    good copy

--- two_pointers/0015_3sum.js
+++ two_pointers/0015_3sum.js
@@ -6,108 +6,30 @@ nums[k] == 0.
 Notice that the solution set must not contain duplicate triplets.
 */
 
-// sorting + nested loop +  binary search
-// time complexity O(nlogn) + O(n^2logn)
+// sorting + two pointer
+// time complexity O(n^2)
 // space complexity O(n)
 var threeSum = function (nums) {
-  let result = [];
-  var mergeSort = function (nums) {
-    var merge = function (left, right) {
-      const array = [];
-      while (left.length && right.length) {
-        if (left[0] < right[0]) {
-          array.push(left.shift());
-        } else {
-          array.push(right.shift());
-        }
-      }
-      return array.concat(left).concat(right);
-    };
-
-    if (nums.length <= 1) {
-      return nums;
-    }
-    let mid = Math.floor(nums.length / 2);
-    let left = nums.slice(0, mid);
-    let right = nums.slice(mid);
-    return merge(mergeSort(left), mergeSort(right));
-  };
-  const sorted = mergeSort(nums);
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i > 0 && sorted[i] === sorted[i - 1]) continue;
-    const twoSum = -sorted[i];
-    const numsUpdated = sorted.slice(i + 1);
-    for (let j = 0; j < nums.length - 2 - i; j++) {
-      if (j > 0 && numsUpdated[j] === numsUpdated[j - 1]) continue;
-      const target = twoSum - numsUpdated[j];
-      const finalArr = numsUpdated.slice(j + 1);
-      let left = 0;
-      let right = finalArr.length - 1;
-      while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-        if (finalArr[mid] > target) {
-          right = mid - 1;
-        } else if (finalArr[mid] < target) {
-          left = mid + 1;
-        } else {
-          result.push([sorted[i], numsUpdated[j], finalArr[mid]]);
-          break;
-        }
-      }
-    }
-  }
-  return result;
-};
-
-// merge sort + two pointers
-// time complexity O(nlogn) + O(n^2)
-// space complexity O(n)
-var threeSum = function (nums) {
-  let result = [];
-  var mergeSort = function (nums) {
-    var merge = function (left, right) {
-      const array = [];
-      while (left.length && right.length) {
-        if (left[0] < right[0]) {
-          array.push(left.shift());
-        } else {
-          array.push(right.shift());
-        }
-      }
-      return array.concat(left).concat(right);
-    };
-
-    if (nums.length <= 1) {
-      return nums;
-    }
-    let mid = Math.floor(nums.length / 2);
-    let left = nums.slice(0, mid);
-    let right = nums.slice(mid);
-    return merge(mergeSort(left), mergeSort(right));
-  };
-  const sorted = mergeSort(nums);
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i > 0 && sorted[i] === sorted[i - 1]) continue;
-    const target = -sorted[i];
-    const sortedU = sorted.slice(i + 1);
-    let left = 0;
-    let right = sortedU.length - 1;
+  let res = [];
+  const n = nums.length;
+  if (n < 3) return res;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < n; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] == nums[i - 1]) continue;
+    let left = i + 1;
+    let right = n - 1;
     while (left < right) {
-      if (sortedU[left] === sortedU[left - 1]) {
+      const sum = nums[i] + nums[left] + nums[right];
+      if (sum == 0) {
+        res.push([nums[i], nums[left], nums[right]]);
+        while (left < right && nums[left] == nums[left + 1]) left++;
+        while (left < right && nums[right] == nums[right - 1]) right--;
         left++;
-      } else if (sortedU[right] === sortedU[right + 1]) {
         right--;
-      } else {
-        if (sortedU[left] + sortedU[right] > target) {
-          right--;
-        } else if (sortedU[left] + sortedU[right] < target) {
-          left++;
-        } else {
-          result.push([sorted[i], sortedU[left], sortedU[right]]);
-          left++;
-        }
-      }
+      } else if (sum < 0) left++;
+      else if (sum > 0) right--;
     }
   }
-  return result;
+  return res;
 };
