03a282b449d4d6523f18c85b38290e0dfc61b3bc
Shu <luo.s@outlook.com>
2023-06-24 21:48:11 -0800

    good copy

---
+++ array/basics/0031_next_permutation.js
@@ -0,0 +1,81 @@
+/*
+A permutation of an array of integers is an arrangement of its members into a 
+sequence or linear order.
+
+For example, for arr = [1,2,3], the following are all the permutations of arr: 
+[1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+The next permutation of an array of integers is the next lexicographically 
+greater permutation of its integer. More formally, if all the permutations of 
+the array are sorted in one container according to their lexicographical order, 
+then the next permutation of that array is the permutation that follows it in 
+the sorted container. If such arrangement is not possible, the array must be 
+rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+For example, the next permutation of arr = [1,2,3] is [1,3,2].
+Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+Given an array of integers nums, find the next permutation of nums.
+
+The replacement must be in place and use only constant extra memory.
+*/
+// https://leetcode.com/problems/next-permutation/
+
+// // time complexity O(n^2)
+// // space complexity O(n)
+// var nextPermutation = function (nums) {
+//   const n = nums.length;
+//   for (let i = n - 1; i > 0; i--) {
+//     if (nums[i] > nums[i - 1]) {
+//       for (let j = n - 1; j > i - 1; j--) {
+//         if (nums[j] > nums[i - 1]) {
+//           let temp = nums[i - 1];
+//           nums[i - 1] = nums[j];
+//           nums[j] = temp;
+//           break;
+//         }
+//       }
+//       let nums1 = nums.slice(0, i);
+//       let nums2 = nums.slice(i);
+//       nums2.sort((a, b) => a - b);
+//       return nums1.concat(nums2);
+//     } else {
+//       if (i == 1) {
+//         return nums.sort((a, b) => a - b);
+//       }
+//     }
+//   }
+// };
+
+var nextPermutation = function (nums) {
+  const n = nums.length;
+  for (let i = n - 1; i > 0; i--) {
+    if (nums[i] > nums[i - 1]) {
+      for (let j = n - 1; j > i - 1; j--) {
+        if (nums[j] > nums[i - 1]) {
+          let temp = nums[i - 1];
+          nums[i - 1] = nums[j];
+          nums[j] = temp;
+          break;
+        }
+      }
+      console.log(nums, i);
+      for (let j = i; j < n; j++) {
+        for (let k = i; k < n - j + i - 1; k++) {
+          if (nums[k] > nums[k + 1]) {
+            let temp = nums[k];
+            nums[k] = nums[k + 1];
+            nums[k + 1] = temp;
+          }
+        }
+      }
+      return nums;
+    } else {
+      if (i == 1) {
+        return nums.sort((a, b) => a - b);
+      }
+    }
+  }
+};
+
+// console.log(nextPermutation([1, 3, 2])); // [2,1,3]
+console.log(nextPermutation([5, 4, 7, 5, 3, 2])); // [5,5,2,3,4,7]
