930ff29aa743ca675b34c1b1282ed97d68b1351f
Shu <luo.s@outlook.com>
2023-07-11 06:00:43 -0800

    good copy

--- sliding_window/0003_longest_substr_without_repeating.js
+++ sliding_window/0003_longest_substr_without_repeating.js
@@ -51,6 +51,27 @@ var lengthOfLongestSubstring = function (s) {
   return result;
 };
 
+// sliding window + Set
+// time complexity O(n)
+// space complexity O(n)
+var lengthOfLongestSubstring = function (s) {
+  const chars = new Set();
+  let left = 0;
+  let right = 0;
+  let max = 0;
+  while (right < s.length) {
+    if (!chars.has(s[right])) {
+      chars.add(s[right]);
+      right++;
+      max = Math.max(max, chars.size);
+    } else {
+      chars.delete(s[left]);
+      left++;
+    }
+  }
+  return max;
+};
+
 console.log(lengthOfLongestSubstring("abcabcbb")); // 3
 console.log(lengthOfLongestSubstring("bbbbb")); // 1
 console.log(lengthOfLongestSubstring("pwwkew")); // 3
