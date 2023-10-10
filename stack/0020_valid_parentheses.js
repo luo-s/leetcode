ccd7d1793fceaf69b2cb3c39e883a68c1ef12351
Shu <luo.s@outlook.com>
2023-10-09 23:18:15 -0400

    stack data structure

---
+++ stack/0020_valid_parentheses.js
@@ -0,0 +1,38 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and 
+']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+s consists of parentheses only '()[]{}'.
+*/
+
+// FILO -> utilizing data structure: stack
+// time complexity O(n)
+// space complexity O(n)
+function isValid(s) {
+  if (s.length % 2) return false;
+  let stack = [];
+  for (let par of s) {
+    switch (par) {
+      case "(":
+      case "{":
+      case "[":
+        stack.push(par);
+        break;
+      case ")":
+        if (stack.pop() !== "(") return false;
+        break;
+      case "]":
+        if (stack.pop() !== "[") return false;
+        break;
+      case "}":
+        if (stack.pop() !== "{") return false;
+        break;
+    }
+  }
+  return !stack.length;
+}
