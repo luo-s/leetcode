b9452cd5cde200e2eb09452f32ca9754db006717
Shu <luo.s@outlook.com>
2023-10-12 21:43:19 -0400

    ternary & binary search

--- binary_search/1342_number_of_steps_to_reduce_a_number_to_zero.js
+++ binary_search/1342_number_of_steps_to_reduce_a_number_to_zero.js
@@ -5,15 +5,14 @@ In one step, if the current number is even, you have to divide it by 2,
 otherwise, you have to subtract 1 from it.
 */
 
-// binary search
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+
+// time complexity O(logn)
+// space complexity O(1)
 var numberOfSteps = function (num) {
   var count = 0;
   while (num !== 0) {
-    if (num % 2 === 0) {
-      num /= 2;
-    } else if (num % 2 === 1) {
-      num -= 1;
-    }
+    num = num % 2 ? num - 1 : num / 2;
     count += 1;
   }
   return count;
