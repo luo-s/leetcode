3d1449acf59fbbccc51d9c09905bc7563fda4e64
Shu <luo.s@outlook.com>
2023-10-17 13:44:21 -0400

    traverse + .sort()

--- array/basics/0088_merge_sorted_array.js
+++ array/basics/0088_merge_sorted_array.js
@@ -21,7 +21,9 @@ var merge = function (nums1, m, nums2, n) {
   nums1.sort((a, b) => a - b);
 };
 
-// brute force
+// traverse + .sort()
+// time complexity O(nlogn)
+// space complexity O(1)
 var merge = function (nums1, m, nums2, n) {
   for (let i = m, j = 0; j < n; i++, j++) {
     nums1[i] = nums2[j];
