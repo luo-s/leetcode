2dc12bba60cca1cd923ecb9288135447b294850c
Shu <luo.s@outlook.com>
2023-10-17 13:41:52 -0400

    higher order functions/ES6

--- array/basics/0088_merge_sorted_array.js
+++ array/basics/0088_merge_sorted_array.js
@@ -13,6 +13,14 @@ of n.
 */
 // https://leetcode.com/problems/merge-sorted-array/
 
+// higher order functions/ES6
+// time complexity O(nlogn)
+// space complexity O(1)
+var merge = function (nums1, m, nums2, n) {
+  nums1.splice(m, n, ...nums2);
+  nums1.sort((a, b) => a - b);
+};
+
 // brute force
 var merge = function (nums1, m, nums2, n) {
   for (let i = m, j = 0; j < n; i++, j++) {
