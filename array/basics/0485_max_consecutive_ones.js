8786016ee7ecc6ada1474c260c222f6951b980a5
Shu <luo.s@outlook.com>
2023-10-17 13:33:30 -0400

    traverse

--- array/basics/0485_max_consecutive_ones.js
+++ array/basics/0485_max_consecutive_ones.js
@@ -5,7 +5,9 @@ in the array.
 
 // http://leetcode.com/problems/max-consecutive-ones/
 
-// better solution O(n)
+// traverse
+// time compelxity O(n)
+// space complexity O(1)
 var findMaxConsecutiveOnes = function (nums) {
   let count = 0;
   let maxCount = 0;
@@ -19,25 +21,3 @@ var findMaxConsecutiveOnes = function (nums) {
   }
   return Math.max(count, maxCount);
 };
-
-//brute force O(n^2)
-var findMaxConsecutiveOnes = function (nums) {
-  let result = 0;
-  for (let i = 0; i < nums.length; i++) {
-    let count = 0;
-    for (let j = i; j < nums.length; j++) {
-      if (nums[j] === 1) {
-        count++;
-      } else {
-        break;
-      }
-    }
-    if (count > result) {
-      result = count;
-    }
-  }
-  return result;
-};
-
-console.log(findMaxConsecutiveOnes([1, 1, 0, 1, 1, 1])); // 3
-console.log(findMaxConsecutiveOnes([1, 0, 1, 1, 0, 1])); // 2
