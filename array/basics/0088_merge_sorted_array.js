7fe909c9e117021d53a17f0ad20c994af5ca3e61
Shu <luo.s@outlook.com>
2023-10-17 13:33:02 -0400

    traverse

---
+++ array/basics/0088_merge_sorted_array.js
@@ -0,0 +1,44 @@
+/*
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing 
+order, and two integers m and n, representing the number of elements in nums1 
+and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead 
+be stored inside the array nums1. To accommodate this, nums1 has a length of 
+m + n, where the first m elements denote the elements that should be merged, 
+and the last n elements are set to 0 and should be ignored. nums2 has a length 
+of n.
+*/
+// https://leetcode.com/problems/merge-sorted-array/
+
+// brute force
+var merge = function (nums1, m, nums2, n) {
+  for (let i = m, j = 0; j < n; i++, j++) {
+    nums1[i] = nums2[j];
+  }
+  nums1.sort((a, b) => a - b);
+  return nums1;
+};
+
+// two pointers
+// time complexity O(m+n)
+// space complexity O(1)
+var merge = function (nums1, m, nums2, n) {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (j >= 0) {
+    if (i >= 0 && nums1[i] > nums2[j]) {
+      nums1[k--] = nums1[i--];
+    } else {
+      nums1[k--] = nums2[j--];
+    }
+  }
+  return nums1;
+};
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
+// console.log(merge([4, 5, 6, 0, 0, 0], 3, [1, 2, 3], 3));
