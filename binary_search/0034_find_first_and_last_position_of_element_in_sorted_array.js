d2af80c7f3524530ecde18774335b6c58c4080ce
Shu <luo.s@outlook.com>
2023-10-18 19:23:07 -0400

    brute force

--- binary_search/0034_find_first_and_last_position_of_element_in_sorted_array.js
+++ binary_search/0034_find_first_and_last_position_of_element_in_sorted_array.js
@@ -10,8 +10,20 @@ You must write an algorithm with O(log n) runtime complexity.
 */
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
-var searchRange = function (nums, target) {};
+// brute force traverse
+// time complexity O(n)
+// space complexity O(1)
+var searchRange = function (nums, target) {
+  let first = -1,
+    last = -1;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === target && nums[i - 1] !== target) first = i;
+    if (nums[i] === target && nums[i + 1] !== target) last = i;
+  }
+  return [first, last];
+};
 
 console.log(searchRange([5, 7, 7, 8, 8, 10], 8)); // [3, 4];
+console.log(searchRange([5, 7, 7, 8, 8, 10], 7)); // [1, 2];
 console.log(searchRange([5, 7, 7, 8, 8, 10], 6)); // [-1, -1];
 console.log(searchRange([], 0)); // [-1, -1];
