83bcdef906af864f32c4e16d5e3d333d1ba8d2e2
Shu <luo.s@outlook.com>
2023-10-18 19:57:45 -0400

    binary search

--- binary_search/0035_search_insert_position.js
+++ binary_search/0035_search_insert_position.js
@@ -13,30 +13,22 @@ You must write an algorithm with O(log n) runtime complexity.
 // same problem: https://leetcode.com/problems/find-smallest-letter-greater-than-target/description/
 var searchInsert = function (nums, target) {
   let left = 0;
-  let right = nums.length - 1; //[left, right]
+  let right = nums.length - 1;
   while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-    if (nums[mid] < target) {
-      left = mid + 1; //[mid + 1, right]
-    } else {
-      right = mid - 1; //[left, mid - 1]
-    }
-  }
-  return left; //left == right + 1
-};
-
-var searchInsert = function (nums, target) {
-  let left = 0;
-  let right = nums.length; //[left, right)
-  while (left < right) {
+    // search [0, nums.length - 1]
     let mid = (left + right) >> 1;
     if (nums[mid] < target) {
-      left = mid + 1; //[mid + 1, right)
+      // target belongs to [mid + 1, right]
+      left = mid + 1;
+    } else if (nums[mid] > target) {
+      // target belongs to [left, mid - 1]
+      right = mid - 1;
     } else {
-      right = mid; //[left, mid)
+      // target == nums[mid]
+      return mid; // nums contains distince values
     }
   }
-  return left; //left == right, can also return right
+  return left;
 };
 
 console.log(searchInsert([1, 3, 5, 6], 5)); // 2
