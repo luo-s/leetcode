e24a2dc9abbfac5f3565413d8afe56cab5185b26
Shu <luo.s@outlook.com>
2023-10-23 12:53:26 -0400

    slide window + set

---
+++ array/sliding_window/0003_longest_substr_without_repeating.js
@@ -0,0 +1,31 @@
+/*
+Given a string s, find the length of the longest substring without repeating 
+characters.
+*/
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+// sliding window + Set
+// time complexity O(n)
+// space complexity O(n)
+var lengthOfLongestSubstring = function (s) {
+  const chars = new Set();
+  let left = 0;
+  let right = 0;
+  let max = 0;
+  while (right < s.length) {
+    if (!chars.has(s[right])) {
+      chars.add(s[right]);
+      right++;
+      max = Math.max(max, chars.size);
+    } else {
+      chars.delete(s[left]);
+      left++;
+    }
+  }
+  return max;
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb")); // 3
+console.log(lengthOfLongestSubstring("bbbbb")); // 1
+console.log(lengthOfLongestSubstring("pwwkew")); // 3
+console.log(lengthOfLongestSubstring("abccba")); // 3
