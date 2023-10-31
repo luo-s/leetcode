31418a703a654eaba980f80113124555fc8dcd4f
Shu <luo.s@outlook.com>
2023-10-30 21:42:06 -0400

    final commit

--- array/sliding_window/0567_permutation_in_string.js
+++ array/sliding_window/0567_permutation_in_string.js
@@ -7,7 +7,56 @@ In other words, return true if one of s1's permutations is the substring of s2.
 
 // https://leetcode.com/problems/permutation-in-string/
 
-var checkInclusion = function (s1, s2) {};
+// sliding window -- better version
+var checkInclusion = function (s1, s2) {
+  if (s1.length > s2.length) return false;
+  // create dictionary based on array structure
+  const cnt1 = new Array(26).fill(0);
+  const cnt2 = new Array(26).fill(0);
+  for (let i = 0; i < s1.length; i++) {
+    cnt1[s1[i].charCodeAt() - "a".charCodeAt()]++;
+    cnt2[s2[i].charCodeAt() - "a".charCodeAt()]++;
+  }
+  if (cnt1.toString() === cnt2.toString()) return true;
+  for (let i = s1.length; i < s2.length; i++) {
+    cnt2[s2[i].charCodeAt() - "a".charCodeAt()]++;
+    cnt2[s2[i - s1.length].charCodeAt() - "a".charCodeAt()]--;
+    if (cnt1.toString() === cnt2.toString()) return true;
+  }
+  return false;
+};
+
+// sliding window -- optimized
+var checkInclusion = function (s1, s2) {
+  const l1 = s1.length,
+    l2 = s2.length;
+  if (l1 > l2) return false;
+  const cnt = new Array(26).fill(0);
+  for (let i = 0; i < l1; i++) {
+    cnt[s1[i].charCodeAt() - "a".charCodeAt()]++;
+    cnt[s2[i].charCodeAt() - "a".charCodeAt()]--;
+  }
+  // determine the permutation
+  let diff = 0;
+  for (const c of cnt) {
+    if (c !== 0) diff++;
+  }
+  if (diff == 0) return true;
+  // sliding the window, recounting diff each time
+  for (let i = l1; i < l2; i++) {
+    const add = s2[i].charCodeAt() - "a".charCodeAt();
+    const rmv = s2[i - l1].charCodeAt() - "a".charCodeAt();
+    if (add == rmv) continue;
+    if (cnt[add] == 0) diff++;
+    cnt[add]--;
+    if (cnt[add] == 0) diff--;
+    if (cnt[rmv] == 0) diff++;
+    cnt[rmv]++;
+    if (cnt[rmv] == 0) diff--;
+    if (diff == 0) return true;
+  }
+  return false;
+};
 
 // hash table + sliding window
 // time complexity O(n^2)
