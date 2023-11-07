902dd5810b766f1d0be1f76b2099c782a153691f
Shu <luo.s@outlook.com>
2023-11-07 16:29:19 -0500

    final commit

--- linked_list/0206_reverse_linked_list.js
+++ linked_list/0206_reverse_linked_list.js
@@ -14,4 +14,16 @@ reversed list.
  * @param {ListNode} head
  * @return {ListNode}
  */
-var reverseList = function (head) {};
+
+//
+var reverseList = function (head) {
+  let prev = null;
+  let curr = head;
+  while (curr) {
+    const next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  return prev;
+};
