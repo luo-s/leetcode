c28ff64fefae9160ed8a0321cef3679048ff9e5a
Shu Luo <luo.s@outlook.com>
2023-11-14 21:26:50 -0500

    hash table

--- string/basics/0006_zigzag_conversion.js
+++ string/basics/0006_zigzag_conversion.js
@@ -1,15 +1,14 @@
 /*
 The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
-
 P   A   H   N
 A P L S I I G
 Y   I   R
 And then read line by line: "PAHNAPLSIIGYIR"
-
 Write the code that will take a string and make this conversion given a number of rows:
-
 string convert(string s, int numRows);
 */
+
+// https://leetcode.com/problems/zigzag-conversion/description/
 
 // pattern repeat every (2 * numRows - 2);
 // hash table
@@ -34,4 +33,4 @@ var convert = function(s, numRows) {
     return ans.join("");
 };
 
-console.log(convert("PAYPALISHIRING", 1));
+console.log(convert("PAYPALISHIRING", 3));
