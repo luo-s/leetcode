8d26b61a329a2ea121bc2bab5e2eb4e02d9c8dd8
Shu <luo.s@outlook.com>
2023-12-15 21:01:07 -0500

    wip

--- string/pattern_match/0028_find_the_index_of_the_first_occurrence_in_a_string.js
+++ string/pattern_match/0028_find_the_index_of_the_first_occurrence_in_a_string.js
@@ -5,7 +5,6 @@ needle in haystack, or -1 if needle is not part of haystack.
 // https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
 
 // rewrite haystack.indexOf(needle, 0);
-// sliding window
 // time complexity O(l1 * l2)
 // space complexity O(1)
 var strStr = function (haystack, needle) {
@@ -23,5 +22,26 @@ var strStr = function (haystack, needle) {
     }
     p++;
   }
+  return -1;
+};
+
+var strStr = function (haystack, needle) {
+  // corner case
+  if (!haystack || !needle || haystack.length < needle.length) return -1;
+  let i = 0,
+    j = 0;
+  // loop through haystack and needle at the same time
+  while (i < haystack.length && j < needle.length) {
+    if (haystack[i] === needle[j]) {
+      i++;
+      j++;
+    } else {
+      i = i - j + 1; // failed matching, reset i to the next index
+      j = 0; // failed matching, reset j to 0
+    }
+  }
+  // if needle matches, return the index
+  if (j === needle.length) return i - j;
+  // if needle does not match, return -1
   return -1;
 };
