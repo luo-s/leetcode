7445f12bb5c6a3e11e6f4bea1d2abec46b9f1563
Shu Luo <luo.s@outlook.com>
2023-12-17 21:05:49 -0500

    binary search solution

--- string/pattern_match/1668_max_repeating_substring.js
+++ string/pattern_match/1668_max_repeating_substring.js
@@ -36,4 +36,26 @@ var maxRepeating = function(sequence, word) {
     return ans;
 }
 
+// binary search
+// time complexity: O(nlogn)
+// space complexity: O(1)
+var maxRepeating = function(sequence, word) {
+    // corner case
+    if(sequence.length < word.length) return 0;
+    if(sequence === word) return 1;
+    let ans = 0, left = 0, right = Math.floor(sequence.length / word.length);
+    while(left <= right) {
+        let mid = Math.floor((left + right) / 2);
+        if(sequence.includes(word.repeat(mid))) {
+            ans = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return ans;
+}
+
+
+
 console.log(maxRepeating("ababc", "ab")); // 2
