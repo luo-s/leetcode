05d6f79a4d4a42d581a96a274f20b3d2eb321ff6
Shu <luo.s@outlook.com>
2024-01-11 08:36:36 -0500

    recursive

---
+++ tree/binary_tree_postorder_traversal.js
@@ -0,0 +1,22 @@
+/*
+Given the root of a binary tree, return the postorder traversal of its nodes' values.
+
+Definition for a binary tree node:
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+*/
+
+// https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+// recursive solution
+var postorderTraversal = function (root) {
+  if (!root) return [];
+  let result = [];
+  if (root.left) result = result.concat(postorderTraversal(root.left));
+  if (root.right) result = result.concat(postorderTraversal(root.right));
+  result.push(root.val);
+  return result;
+};
