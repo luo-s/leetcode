8626158b8b3dcbe363ef0c921070e3b6212a74c5
Shu <luo.s@outlook.com>
2024-01-13 23:20:02 -0500

    wip

---
+++ tree/0104_maximum_depth_of_binary_tree.js
@@ -0,0 +1,10 @@
+/*
+Given the root of a binary tree, return its maximum depth.
+
+A binary tree's maximum depth is the number of nodes along the longest path 
+from the root node down to the farthest leaf node.
+*/
+
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+var maxDepth = function (root) {};
