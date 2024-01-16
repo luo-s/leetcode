f3aa185ae9472c1c51765e803805972eb4058446
Shu <luo.s@outlook.com>
2024-01-15 22:47:29 -0500

    recursive solution

--- tree/0111_minimum_depth_of_binary_tree.js
+++ tree/0111_minimum_depth_of_binary_tree.js
@@ -9,4 +9,11 @@ Note: A leaf is a node with no children.
 
 // https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
 
-var minDepth = function (root) {};
+// recursive solution
+var minDepth = function (root) {
+  if (!root) return 0;
+  if (!root.left && !root.right) return 1;
+  if (!root.left && root.right) return 1 + minDepth(root.right);
+  if (root.left && !root.right) return 1 + minDepth(root.left);
+  return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+};
