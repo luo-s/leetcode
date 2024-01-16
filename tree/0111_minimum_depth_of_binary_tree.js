d2e19242db381242ffe94a6883f156b372939e03
Shu <luo.s@outlook.com>
2024-01-15 22:36:43 -0500

    wip

---
+++ tree/0111_minimum_depth_of_binary_tree.js
@@ -0,0 +1,12 @@
+/*
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the 
+root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+*/
+
+// https://leetcode.com/problems/minimum-depth-of-binary-tree/description/
+
+var minDepth = function (root) {};
