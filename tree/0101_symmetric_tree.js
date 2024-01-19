7c3990877b770a950436a4c5b7ae29330244fc42
Shu Luo <luo.s@outlook.com>
2024-01-18 20:24:42 -0500

    wip

---
+++ tree/0101_symmetric_tree.js
@@ -0,0 +1,10 @@
+/*
+Given the root of a binary tree, check whether it is 
+a mirror of itself (i.e., symmetric around its center).
+*/
+
+// https://leetcode.com/problems/symmetric-tree/description/
+
+var isSymmetric = function(root) {
+    
+};
