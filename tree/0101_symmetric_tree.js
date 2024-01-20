2d4b82bfc5d0a5af859d049a55caaebae0b10dc6
Shu Luo <luo.s@outlook.com>
2024-01-19 20:53:53 -0500

    recursive solution

--- tree/0101_symmetric_tree.js
+++ tree/0101_symmetric_tree.js
@@ -5,6 +5,15 @@ a mirror of itself (i.e., symmetric around its center).
 
 // https://leetcode.com/problems/symmetric-tree/description/
 
+// recursive solution
 var isSymmetric = function(root) {
-    
+    var isMirror = function(left, right) {
+        if (!left && !right) return true;
+        if (!left || !right) return false;
+        return (left.val === right.val) && isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+    if (!root) return true;
+    return isMirror(root.left, root.right);
+
 };
+
