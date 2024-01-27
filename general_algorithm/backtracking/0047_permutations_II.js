8f9d4a1f17016f0c59ca6c973b7a6790de475907
Shu <luo.s@outlook.com>
2024-01-26 21:48:52 -0500

    wip

--- general_algorithm/backtracking/0047_permutations_II.js
+++ general_algorithm/backtracking/0047_permutations_II.js
@@ -2,3 +2,7 @@
 Given a collection of numbers, nums, that might contain duplicates, 
 return all possible unique permutations in any order.
 */
+var permuteUnique = function (nums) {};
+
+console.log(permuteUnique([1, 2, 3])); // [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+console.log(permuteUnique([1, 1, 2])); // [[1,1,2],[1,2,1],[2,1,1]]
