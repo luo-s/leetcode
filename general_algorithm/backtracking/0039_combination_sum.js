f498f5bddd114c4383725c9ab72f4a3b6ba4981d
Shu <luo.s@outlook.com>
2024-01-28 00:13:40 -0500

    backtracking

--- general_algorithm/backtracking/0039_combination_sum.js
+++ general_algorithm/backtracking/0039_combination_sum.js
@@ -11,3 +11,28 @@ The test cases are generated such that the number of unique combinations that su
 */
 
 // https://leetcode.com/problems/combination-sum/description/
+
+var combinationSum = function (candidates, target) {
+  let result = [];
+  let path = [];
+  candidates.sort((a, b) => a - b);
+  // define backtracking function
+  var backtracking = function (candidates, target, index) {
+    // push current subset to result
+    if (target === 0) {
+      result.push([...path]);
+      return;
+    }
+    // if go too far, return
+    if (target < 0) return;
+    // loop through nums from index not 0 (eleminate duplicate subsets)
+    for (let i = index; i < candidates.length; i++) {
+      path.push(candidates[i]);
+      // not i + 1 because we can reuse same elements
+      backtracking(candidates, target - candidates[i], i);
+      path.pop();
+    }
+  };
+  backtracking(candidates, target, 0);
+  return result;
+};
