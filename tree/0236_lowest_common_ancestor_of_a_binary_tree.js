bb8ff1c9e9cace340f77f2ade7b9bdfbf4372846
Shu <luo.s@outlook.com>
2024-01-30 22:49:28 -0500

    recursive

--- tree/0236_lowest_common_ancestor_of_a_binary_tree.js
+++ tree/0236_lowest_common_ancestor_of_a_binary_tree.js
@@ -15,4 +15,23 @@ p and q will exist in the tree.
 
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
 
-var lowestCommonAncestor = function (root, p, q) {};
+var lowestCommonAncestor = function (root, p, q) {
+  // if root is null, return null
+  if (root === null) {
+    return null;
+  }
+  // if root is p or root is q, root is the LCA
+  if (root === p || root === q) {
+    return root;
+  }
+  // recursively find LCA in left and right subtrees
+  let left = lowestCommonAncestor(root.left, p, q);
+  let right = lowestCommonAncestor(root.right, p, q);
+  // if left and right are both not null, then p and q are separately
+  // in left and right subtrees, thus the root is the LCA
+  if (left !== null && right !== null) {
+    return root;
+  }
+  // if left is null, p and q are both in right subtree, vice versa
+  return left !== null ? left : right;
+};
