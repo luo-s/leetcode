5f8142c792de238122947f5c447631a69c6d1690
Shu Luo <luo.s@outlook.com>
2024-02-01 14:52:02 -0500

    recursive solution

--- tree/binary_search_tree/0701_insert_into_a_binary_search_tree.js
+++ tree/binary_search_tree/0701_insert_into_a_binary_search_tree.js
@@ -5,8 +5,18 @@ It is guaranteed that the new value does not exist in the original BST.
 
 Notice that there may exist multiple valid ways for the insertion, as long as the tree remains a BST after insertion. 
 You can return any of them.
+
+Definition for a binary tree node:
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val)
+    this.left = (left===undefined ? null : left)
+    this.right = (right===undefined ? null : right)
+}
 */
 
 var insertIntoBST = function(root, val) {
-    
+    if (!root) return new TreeNode(val);
+    if (root.val > val) root.left = insertIntoBST(root.left, val);
+    else root.right = insertIntoBST(root.right, val);
+    return root;
 };
