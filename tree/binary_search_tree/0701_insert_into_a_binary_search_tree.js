85720964531ec6ba00b381ab56752b834dc3c68d
Shu <luo.s@outlook.com>
2024-02-02 15:38:26 -0500

    final commit

--- tree/binary_search_tree/0701_insert_into_a_binary_search_tree.js
+++ tree/binary_search_tree/0701_insert_into_a_binary_search_tree.js
@@ -14,9 +14,10 @@ function TreeNode(val, left, right) {
 }
 */
 
-var insertIntoBST = function(root, val) {
-    if (!root) return new TreeNode(val);
-    if (root.val > val) root.left = insertIntoBST(root.left, val);
-    else root.right = insertIntoBST(root.right, val);
-    return root;
+// This method inserts to the leaf node
+var insertIntoBST = function (root, val) {
+  if (!root) return new TreeNode(val);
+  if (root.val > val) root.left = insertIntoBST(root.left, val);
+  else root.right = insertIntoBST(root.right, val);
+  return root;
 };
