63f79488d8f26472f719f8fd90b696883adaa74b
Shu <luo.s@outlook.com>
2024-02-08 09:03:09 -0500

    good copy

--- tree/binary_search_tree/0450_delete_node_in_BST.js
+++ tree/binary_search_tree/0450_delete_node_in_BST.js
@@ -10,4 +10,31 @@ Search for a node to remove.
 If the node is found, delete the node.
 */
 
-var deleteNode = function (root, key) {};
+// move left subtree to the left of the smallest node in the right subtree
+// replace the node with the right subtree
+var deleteNode = function (root, key) {
+  // if the root is null, return null
+  if (!root) return root;
+  if (root.val > key) {
+    // if the root value > key, go left
+    root.left = deleteNode(root.left, key);
+    return root;
+  } else if (root.val < key) {
+    // if the root value < key, go right
+    root.right = deleteNode(root.right, key);
+    return root;
+  } else {
+    // found the matching node
+    if (!root.left) return root.right;
+    if (!root.right) return root.left;
+    // find the smallest node in the right subtree
+    let minRight = root.right;
+    while (minRight.left) {
+      minRight = minRight.left;
+    }
+    // move left subtree to the left of the smallest node in the right subtree
+    minRight.left = root.left;
+    // replace the node with the right subtree
+    return root.right;
+  }
+};
