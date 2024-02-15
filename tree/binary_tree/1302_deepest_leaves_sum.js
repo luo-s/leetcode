6685779fefd177b28b719449553babcc0e3a94db
Shu <luo.s@outlook.com>
2024-02-14 20:40:20 -0500

    wip

---
+++ tree/binary_tree/1302_deepest_leaves_sum.js
@@ -0,0 +1,7 @@
+/*
+Given the root of a binary tree, return the sum of values of its deepest leaves.
+*/
+
+// https://leetcode.com/problems/deepest-leaves-sum/description/
+
+var deepestLeavesSum = function (root) {};
