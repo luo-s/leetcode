6a3b49e17c26812fc81ae82fd24c83e8ca60628f
Shu <luo.s@outlook.com>
2024-02-14 21:05:27 -0500

    dfs

--- tree/binary_tree/1302_deepest_leaves_sum.js
+++ tree/binary_tree/1302_deepest_leaves_sum.js
@@ -4,4 +4,16 @@ Given the root of a binary tree, return the sum of values of its deepest leaves.
 
 // https://leetcode.com/problems/deepest-leaves-sum/description/
 
-var deepestLeavesSum = function (root) {};
+// depth first search
+var deepestLeavesSum = function (root) {
+  let depthSum = new Map();
+  var dfs = function (node, depth) {
+    if (node.left) dfs(node.left, depth + 1);
+    if (node.right) dfs(node.right, depth + 1);
+    if (!node.left && !node.right) {
+      depthSum.set(depth, (depthSum.get(depth) || 0) + node.val);
+    }
+  };
+  dfs(root, 0);
+  return depthSum.get(Math.max(...depthSum.keys()));
+};
