5449aec38128f8ece87bd17cad2d7432e2b84ae9
Shu <luo.s@outlook.com>
2024-02-20 08:11:39 -0500

    wip

---
+++ array/basics/0989_add_to_array_form_of_integer.js
@@ -0,0 +1,17 @@
+/*
+The array-form of an integer num is an array representing its digits in 
+left to right order.
+
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, 
+return the array-form of the integer num + k.
+
+1 <= num.length <= 10^4
+0 <= num[i] <= 9
+num does not contain any leading zeros except for the zero itself.
+1 <= k <= 10^4
+*/
+
+// https://leetcode.com/problems/add-to-array-form-of-integer/
+
+var addToArrayForm = function (num, k) {};
