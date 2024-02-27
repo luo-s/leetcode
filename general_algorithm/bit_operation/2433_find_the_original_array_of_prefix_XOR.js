1e0fae9c9bbf5e24c35b58255ff965cac8bfd770
Shu <luo.s@outlook.com>
2024-02-26 21:24:29 -0500

    wip

---
+++ general_algorithm/bit_operation/2433_find_the_original_array_of_prefix_XOR.js
@@ -0,0 +1,13 @@
+/*
+You are given an integer array pref of size n. 
+Find and return the array arr of size n that satisfies:
+
+pref[i] = arr[0] ^ arr[1] ^ ... ^ arr[i].
+Note that ^ denotes the bitwise-xor operation.
+
+It can be proven that the answer is unique.
+*/
+
+// https://leetcode.com/problems/find-the-original-array-of-prefix-xor/description/
+
+var findArray = function (pref) {};
