856d5dddf2f3bd6172b0dc8e383ea91242f3019c
Shu <luo.s@outlook.com>
2024-02-27 09:15:24 -0500

    optimized

--- general_algorithm/bit_operation/1829_maximum_XOR_for_each_query.js
+++ general_algorithm/bit_operation/1829_maximum_XOR_for_each_query.js
@@ -16,6 +16,8 @@ nums​​​ is sorted in ascending order.
 */
 
 // brute force  -- time limit exceeded
+// time complexity: O(n * (2 ** maximumBit))
+// spapce complexity: O(n)
 var getMaximumXor = function (nums, maximumBit) {
   let prefix = new Array(nums.length).fill(0);
   nums.forEach((ele, i) => {
@@ -36,4 +38,52 @@ var getMaximumXor = function (nums, maximumBit) {
     answer.push(index);
   }
   return answer;
+};
+
+// bit manipulation
+// for each query: maxmize prefix[i] ^ k, where 0 <= k < 2 ** maximumBit
+// 0 <= nums[i] < 2 ** maximumBit -> 0 <= prefix[i] < 2 ** maximumBit
+// thus the max value of prefix[i] ^ k is 2 ** maximumBit - 1
+// k = (2 ** maximumBit - 1) ^ prefix[i]
+// time complexity: O(n)
+// space complexity: O(n)
+var getMaximumXor = function (nums, maximumBit) {
+  let prefix = new Array(nums.length).fill(0);
+  nums.forEach((ele, i) => {
+    prefix[i] = (prefix[i - 1] || 0) ^ ele;
+  });
+  let answer = [],
+    max = Math.pow(2, maximumBit) - 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    const index = max ^ prefix[i];
+    answer.push(index);
+  }
+  return answer;
+};
+
+// optimized bit manipulation
+// time complexity: O(n)
+// space complexity: O(n)
+var getMaximumXor = function (nums, maximumBit) {
+  let max = 2 ** maximumBit - 1,
+    xorSum = 0;
+  result = new Array(nums.length);
+  for (let i = 0; i < nums.length; i++) {
+    xorSum ^= nums[i];
+    result[nums.length - 1 - i] = max ^ xorSum;
+  }
+  return result;
+};
+
+// optimized bit manipulation
+var getMaximumXor = function (nums, maximumBit) {
+  let max = (1 << maximumBit) - 1;
+  // loop through the nums array
+  for (let i = 0; i < nums.length; i++) {
+    // update the max value
+    max ^= nums[i];
+    // the current max if the (nums.length - 1 - i)th query
+    nums[i] = max;
+  }
+  return nums.reverse();
 };
