02c989269d9301ea25a8d81038e26685a917719a
Shu <luo.s@outlook.com>
2024-02-26 21:36:00 -0500

    good copy

--- general_algorithm/bit_operation/2433_find_the_original_array_of_prefix_XOR.js
+++ general_algorithm/bit_operation/2433_find_the_original_array_of_prefix_XOR.js
@@ -10,4 +10,12 @@ It can be proven that the answer is unique.
 
 // https://leetcode.com/problems/find-the-original-array-of-prefix-xor/description/
 
-var findArray = function (pref) {};
+// pref[i] == pref[i-1] ^ res[i]
+// res[i] == pref[i] ^ p[i-1]
+var findArray = function (pref) {
+  let res = [pref[0]];
+  for (let i = 1; i < pref.length; i++) {
+    res.push(pref[i] ^ pref[i - 1]);
+  }
+  return res;
+};
