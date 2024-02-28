bc05f3d41df5392042f63a87ad283d81fc518a04
Shu <luo.s@outlook.com>
2024-02-27 21:44:40 -0500

    wip

---
+++ general_algorithm/bit_operation/2429_minimize_XOR.js
@@ -0,0 +1,16 @@
+/*
+Given two positive integers num1 and num2, find the positive integer x such that:
+
+x has the same number of set bits as num2, and
+The value x XOR num1 is minimal.
+Note that XOR is the bitwise XOR operation.
+
+Return the integer x. The test cases are generated such that x is uniquely determined.
+
+The number of set bits of an integer is the number of 1's in its binary representation.
+1 <= num1, num2 <= 10 ** 9
+*/
+
+// https://leetcode.com/problems/minimize-xor/description/
+
+var minimizeXor = function (num1, num2) {};
