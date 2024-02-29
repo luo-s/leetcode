7bee1a08720f9b3205f64c3dbb052c3198802e4e
Shu <luo.s@outlook.com>
2024-02-29 08:24:30 -0500

    memoization

--- dynamic_programming/0509_fibonacci_number.js
+++ dynamic_programming/0509_fibonacci_number.js
@@ -8,7 +8,19 @@ Given n, calculate F(n).
 */
 // https://leetcode.com/problems/fibonacci-number/description/
 
-// dp
+// recursion
+// really slow: a lot of repeated calculations, huge call stack
+// time complexity: O(2^n)
+// space complexity: O(1)
+var fib_rc = function (n) {
+  if (n == 0) return 0;
+  if (n == 1) return 1;
+  return fib_rc(n - 1) + fib_rc(n - 2);
+};
+
+// tabulation: bottom-up dynamic programming
+// time complexity: O(n)
+// space complexity: O(n)
 var fib_dp = function (n) {
   const dp = new Array(n + 1).fill(0);
   dp[1] = 1;
@@ -18,18 +30,33 @@ var fib_dp = function (n) {
   return dp[n];
 };
 
-console.time("dynamic programming method timer");
-let ans_1 = fib_dp(42);
-console.log(`dynamic programming method: ${ans_1}`);
-console.timeEnd("dynamic programming method timer");
+// memoization: top-down dynamic programming
+// time complexity: O(n)
+// space complexity: O(n)
+var fib_memo = function (n) {
+  const memo = new Map();
+  return helper(n, memo);
+};
 
-var fib_rc = function (n) {
+var helper = function (n, memo) {
   if (n == 0) return 0;
   if (n == 1) return 1;
-  return fib_rc(n - 1) + fib_rc(n - 2);
+  if (memo.has(n)) return memo.get(n);
+  memo.set(n, helper(n - 1, memo) + helper(n - 2, memo));
+  return memo.get(n);
 };
 
 console.time("recursive method timer");
 let ans_2 = fib_rc(42);
 console.log(`recursion method: ${ans_2}`);
 console.timeEnd("recursive method timer");
+
+console.time("tabulation method timer");
+let ans_1 = fib_dp(42);
+console.log(`tabulation method: ${ans_1}`);
+console.timeEnd("tabulation method timer");
+
+console.time("memoization method timer");
+let ans_3 = fib_memo(42);
+console.log(`memoization method: ${ans_3}`);
+console.timeEnd("memoization method timer");
