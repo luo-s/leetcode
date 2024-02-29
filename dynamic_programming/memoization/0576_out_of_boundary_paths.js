d40d60c8ce96d34d32816109787739c49f273d4c
Shu <luo.s@outlook.com>
2024-02-29 13:22:43 -0500

    memoization

--- dynamic_programming/memoization/0576_out_of_boundary_paths.js
+++ dynamic_programming/memoization/0576_out_of_boundary_paths.js
@@ -17,7 +17,7 @@ Since the answer can be very large, return it modulo (10^9 + 7).
 
 // dfs
 var findPaths = function (m, n, maxMove, startRow, startColumn) {
-  let count = 0;
+  let count = BigInt(0);
   var dfs = (startRow, startColumn, maxMove) => {
     if (maxMove < 0) return;
     if (startRow < 0 || startRow >= m || startColumn < 0 || startColumn >= n) {
@@ -30,5 +30,26 @@ var findPaths = function (m, n, maxMove, startRow, startColumn) {
     dfs(startRow, startColumn + 1, maxMove - 1);
   };
   dfs(startRow, startColumn, maxMove);
-  return count % (10 ** 9 + 7);
+  return count % BigInt(10 ** 9 + 7);
+};
+
+// dfs with memoization
+var findPaths = function (m, n, maxMove, startRow, startColumn) {
+  let memo = new Map();
+  var dfs = (startRow, startColumn, maxMove) => {
+    if (maxMove < 0) return 0;
+    if (startRow < 0 || startRow >= m || startColumn < 0 || startColumn >= n) {
+      return 1;
+    }
+    let key = `${startRow}-${startColumn}-${maxMove}`;
+    if (memo.has(key)) return memo.get(key);
+    let count = BigInt(0);
+    count += BigInt(dfs(startRow - 1, startColumn, maxMove - 1));
+    count += BigInt(dfs(startRow + 1, startColumn, maxMove - 1));
+    count += BigInt(dfs(startRow, startColumn - 1, maxMove - 1));
+    count += BigInt(dfs(startRow, startColumn + 1, maxMove - 1));
+    memo.set(key, count);
+    return count % BigInt(10 ** 9 + 7);
+  };
+  return dfs(startRow, startColumn, maxMove);
 };
