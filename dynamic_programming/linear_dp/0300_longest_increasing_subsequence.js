8a51d4bcf9d3efd7f72bc9d99bffb09890d5d379
Shu Luo <luo.s@outlook.com>
2024-02-29 18:21:22 -0500

    dp

--- dynamic_programming/linear_dp/0300_longest_increasing_subsequence.js
+++ dynamic_programming/linear_dp/0300_longest_increasing_subsequence.js
@@ -11,4 +11,26 @@ some or no elements without changing the order of the remaining elements.
 
 // https://leetcode.com/problems/longest-increasing-subsequence/
 
-var lengthOfLIS = function (nums) {};
+
+
+// dynamic programming
+// for every j that 0 <= j < i; if nums[j] < nums[i], dp[i] = max(dp[i], dp[j]+1)
+// time complexity: O(n^2)
+// space complexity: O(n)
+var lengthOfLIS = function(nums) {
+    // dp[i] represents the length of the longest increasing subsequence that ends with nums[i]
+    const dp = new Array(nums.length).fill(1);
+    for (let i = 1; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+    return Math.max(...dp);
+};
+
+console.log(lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]) === 4);
+// console.log(lengthOfLIS([0, 1, 3, 5, 2, 3]) === 4);
+// console.log(lengthOfLIS([7, 7, 7, 7, 7, 7, 7]) === 1);
+// console.log(lengthOfLIS([4, 10, 9, 3, 8, 9]) === 3);
