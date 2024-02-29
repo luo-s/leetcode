b702f86cf4cff2b54024514a1b99862c182555d6
Shu <luo.s@outlook.com>
2024-02-29 15:57:25 -0500

    wip

---
+++ dynamic_programming/linear_dp/0300_longest_increasing_subsequence.js
@@ -0,0 +1,14 @@
+/*
+Given an integer array nums, return the length of the longest strictly 
+increasing subsequence.
+
+A subsequence is an array that can be derived from another array by deleting 
+some or no elements without changing the order of the remaining elements.
+
+1 <= nums.length <= 2500
+-10^4 <= nums[i] <= 10^4
+*/
+
+// https://leetcode.com/problems/longest-increasing-subsequence/
+
+var lengthOfLIS = function (nums) {};
