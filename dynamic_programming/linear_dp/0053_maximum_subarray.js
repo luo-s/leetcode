0e42356181b31a8687fc6ca20af1a40690cbcb3a
Shu <luo.s@outlook.com>
2024-03-01 06:55:42 -0500

    brute force

--- dynamic_programming/linear_dp/0053_maximum_subarray.js
+++ dynamic_programming/linear_dp/0053_maximum_subarray.js
@@ -7,4 +7,18 @@ A subarray is a contiguous non-empty sequence of elements within an array.
 1 <= nums.length <= 10^5
 -10^4 <= nums[i] <= 10^4
 */
-var maxSubArray = function (nums) {};
+
+// brute force -- TLE
+// time complexity: O(n^2)
+// space complexity: O(1)
+var maxSubArray = function (nums) {
+  let max = -Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    let sum = 0;
+    for (let j = i; j < nums.length; j++) {
+      sum += nums[j];
+      max = Math.max(max, sum);
+    }
+  }
+  return max;
+};
