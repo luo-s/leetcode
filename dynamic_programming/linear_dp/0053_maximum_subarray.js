de63bf1bdcb793d3ac8d7ac908404a2c767e3a91
Shu <luo.s@outlook.com>
2024-03-01 06:47:51 -0500

    wip

---
+++ dynamic_programming/linear_dp/0053_maximum_subarray.js
@@ -0,0 +1,10 @@
+/*
+Given an integer array nums, find the subarray with the largest sum, 
+and return its sum.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+1 <= nums.length <= 10^5
+-10^4 <= nums[i] <= 10^4
+*/
+var maxSubArray = function (nums) {};
