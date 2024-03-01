83380f35eba89ffb541029e3a29a352e3482fb42
Shu <luo.s@outlook.com>
2024-03-01 09:01:36 -0500

    good copy

--- dynamic_programming/linear_dp/1143_longest_common_subsequence.js
+++ dynamic_programming/linear_dp/1143_longest_common_subsequence.js
@@ -15,4 +15,24 @@ text1 and text2 consist of only lowercase English characters.
 
 // https://leetcode.com/problems/longest-common-subsequence/
 
-var longestCommonSubsequence = function (text1, text2) {};
+// let dp[i][j] be the length of the longest common subsequence of
+// text1[0:i) and text2[0:j)
+// if text1[i] === text2[j], then dp[i][j] = dp[i - 1][j - 1] + 1
+// else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+var longestCommonSubsequence = function (text1, text2) {
+  let l1 = text1.length,
+    l2 = text2.length;
+  let dp = Array(l1 + 1)
+    .fill()
+    .map(() => Array(l2 + 1).fill(0));
+  for (let i = 1; i <= l1; i++) {
+    for (let j = 1; j <= l2; j++) {
+      if (text1[i - 1] === text2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[l1][l2];
+};
