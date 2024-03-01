25d36903ff2bab1fecd62dbb0cb4dc6092a8c240
Shu <luo.s@outlook.com>
2024-03-01 09:04:30 -0500

    wip

---
+++ dynamic_programming/linear_dp/0718_longest_repeated_subarray.js
@@ -0,0 +1,8 @@
+/*
+Given two integer arrays nums1 and nums2, return the maximum length of a 
+subarray that appears in both arrays.
+*/
+
+// https://leetcode.com/problems/maximum-length-of-repeated-subarray/description/
+
+var findLength = function (nums1, nums2) {};
