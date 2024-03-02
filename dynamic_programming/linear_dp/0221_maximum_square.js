de89366925fba39a4531b93ce96e9788d076e28d
Shu <luo.s@outlook.com>
2024-03-02 00:06:48 -0500

    wip

---
+++ dynamic_programming/linear_dp/0221_maximum_square.js
@@ -0,0 +1,8 @@
+/*
+Given an m x n binary matrix filled with 0's and 1's, find the largest 
+square containing only 1's and return its area.
+*/
+
+// https://leetcode.com/problems/maximal-square/description/
+
+var maximalSquare = function (matrix) {};
