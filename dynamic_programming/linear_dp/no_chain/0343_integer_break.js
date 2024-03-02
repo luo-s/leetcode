1614246d064fdb77f223a50a431041bb940471a1
Shu <luo.s@outlook.com>
2024-03-02 14:24:52 -0500

    dynamic programming

--- dynamic_programming/linear_dp/no_chain/0343_integer_break.js
+++ dynamic_programming/linear_dp/no_chain/0343_integer_break.js
@@ -8,4 +8,19 @@ Return the maximum product you can get.
 
 // https://leetcode.com/problems/integer-break/description/
 
-var integerBreak = function (n) {};
+// let dp[i] be the maximum product of breaking i
+// for every j that, 0 < j < i, we can break i into j and i - j
+// we further break j into two parts, if and only if dp[j] > j
+// we further break i - j into two parts, if and only if dp[i - j] > i - j
+var integerBreak = function (n) {
+  let dp = new Array(n + 1).fill(0);
+  for (let i = 2; i <= n; i++) {
+    // the minimum product of breaking i is 1 * (i - 1)
+    dp[i] = 1 * (i - 1);
+    let mid = Math.floor(i / 2);
+    for (let j = 1; j <= mid; j++) {
+      dp[i] = Math.max(dp[i], Math.max(j, dp[j]) * Math.max(i - j, dp[i - j]));
+    }
+  }
+  return dp[n];
+};
