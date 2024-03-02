2ee9190e179da1d71b7c66776350694d0f35a9b6
Shu <luo.s@outlook.com>
2024-03-02 11:02:23 -0500

    brute force

--- dynamic_programming/linear_dp/0221_maximum_square.js
+++ dynamic_programming/linear_dp/0221_maximum_square.js
@@ -5,4 +5,33 @@ square containing only 1's and return its area.
 
 // https://leetcode.com/problems/maximal-square/description/
 
-var maximalSquare = function (matrix) {};
+// brute force -- time limit exceeded
+// time complexity: O(m * n * min(m, n)^2)
+var maximalSquare = function (matrix) {
+  var isSquare = function (matrix, i, j, k) {
+    // start from [i, j], stop at [i + k, j + k]
+    // check square boundary
+    if (i + k >= matrix.length) return false;
+    if (j + k >= matrix[0].length) return false;
+    // check square content
+    for (let x = i; x <= i + k; x++) {
+      if (j + k >= matrix[0].length) return false;
+      for (let y = j; y <= j + k; y++) {
+        if (matrix[x][y] === "0") return false;
+      }
+    }
+    return true;
+  };
+  let max = 0;
+  let limit = Math.min(matrix.length, matrix[0].length);
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      for (let k = 0; k < limit; k++) {
+        if (isSquare(matrix, i, j, k)) {
+          max = Math.max(max, k + 1);
+        }
+      }
+    }
+  }
+  return max ** 2;
+};
