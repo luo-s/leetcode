363a9c4d6852fc047c5fc26e61d55b4943382635
Shu <luo.s@outlook.com>
2024-03-02 12:33:33 -0500

    good copy

--- dynamic_programming/linear_dp/0221_maximum_square.js
+++ dynamic_programming/linear_dp/0221_maximum_square.js
@@ -1,6 +1,11 @@
 /*
 Given an m x n binary matrix filled with 0's and 1's, find the largest 
 square containing only 1's and return its area.
+
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 300
+matrix[i][j] is '0' or '1'.
 */
 
 // https://leetcode.com/problems/maximal-square/description/
@@ -30,6 +35,27 @@ var maximalSquare = function (matrix) {
         if (isSquare(matrix, i, j, k)) {
           max = Math.max(max, k + 1);
         }
+      }
+    }
+  }
+  return max ** 2;
+};
+
+// let dp[i][j] be the max side length where [i, j] is the bottom right corner
+// if matrix[i][j] === 0, dp[i][j] = 0
+// if matrix[i][j] === 1, dp[i][j] = min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1]) + 1
+var maximalSquare = function (matrix) {
+  let m = matrix.length,
+    n = matrix[0].length;
+  let dp = new Array(m + 1).fill().map(() => new Array(n + 1).fill(0));
+  let max = 0;
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (matrix[i - 1][j - 1] === "0") {
+        dp[i][j] = 0;
+      } else {
+        dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1;
+        max = Math.max(max, dp[i][j]);
       }
     }
   }
