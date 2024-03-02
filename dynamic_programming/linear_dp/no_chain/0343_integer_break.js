bb1dba76e8da040b9a50af460452c3c9da891c57
Shu <luo.s@outlook.com>
2024-03-02 14:05:26 -0500

    wip

---
+++ dynamic_programming/linear_dp/no_chain/0343_integer_break.js
@@ -0,0 +1,11 @@
+/*
+Given an integer n, break it into the sum of k positive integers, 
+where k >= 2, and maximize the product of those integers.
+
+Return the maximum product you can get.
+2 <= n <= 58
+*/
+
+// https://leetcode.com/problems/integer-break/description/
+
+var integerBreak = function (n) {};
