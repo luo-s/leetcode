215db728214185c4db592c7aa5dac5b1843a73a4
Shu <luo.s@outlook.com>
2024-03-02 00:18:14 -0500

    good copy

--- dynamic_programming/linear_dp/0064_minimum_path_sum.js
+++ dynamic_programming/linear_dp/0064_minimum_path_sum.js
@@ -7,4 +7,17 @@ Note: You can only move either down or right at any point in time.
 
 // https://leetcode.com/problems/minimum-path-sum/
 
-var minPathSum = function (grid) {};
+// let dp[i][j] be the minimum path sum to reach [i, j]
+// dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+var minPathSum = function (grid) {
+  let dp = new Array(grid.length + 1)
+    .fill()
+    .map(() => new Array(grid[0].length + 1).fill(Infinity));
+  for (let i = 1; i < grid.length + 1; i++) {
+    for (let j = 1; j < grid[0].length + 1; j++) {
+      if (i === 1 && j === 1) dp[i][j] = grid[i - 1][j - 1];
+      else dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+    }
+  }
+  return dp[grid.length][grid[0].length];
+};
