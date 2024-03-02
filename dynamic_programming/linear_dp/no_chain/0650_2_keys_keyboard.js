0efe0686cb649d997c522bcbdae1abfc197c650e
Shu <luo.s@outlook.com>
2024-03-02 17:19:55 -0500

    wip

---
+++ dynamic_programming/linear_dp/no_chain/0650_2_keys_keyboard.js
@@ -0,0 +1,14 @@
+/*
+There is only one character 'A' on the screen of a notepad. You can perform 
+one of two operations on this notepad for each step:
+
+1. Copy All: You can copy all the characters present on the screen.
+2. Paste: You can paste the characters which are copied last time.
+
+Given an integer n, return the minimum number of operations to get the 
+character 'A' exactly n times on the screen.
+*/
+
+// https://leetcode.com/problems/2-keys-keyboard/description/
+
+var minSteps = function (n) {};
