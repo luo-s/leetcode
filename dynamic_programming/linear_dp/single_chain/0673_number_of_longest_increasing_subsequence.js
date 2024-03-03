0235fa1eb122f675a71f6c94a6ae6753c9d094fc
Shu <luo.s@outlook.com>
2024-03-03 00:19:39 -0500

    wip

---
+++ dynamic_programming/linear_dp/single_chain/0673_number_of_longest_increasing_subsequence.js
@@ -0,0 +1,11 @@
+/*
+Given an integer array nums, return the number of longest increasing 
+subsequences.
+
+Notice that the sequence has to be strictly increasing.
+
+1 <= nums.length <= 2000
+-10^6 <= nums[i] <= 10^6
+*/
+
+var findNumberOfLIS = function (nums) {};
