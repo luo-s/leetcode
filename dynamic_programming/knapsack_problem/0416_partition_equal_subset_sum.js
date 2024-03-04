80ba414e39079cc6075ac666fe196c84112fc134
Shu <luo.s@outlook.com>
2024-03-04 13:26:04 -0500

    wip

---
+++ dynamic_programming/knapsack_problem/0416_partition_equal_subset_sum.js
@@ -0,0 +1,13 @@
+/*
+Given an integer array nums, return true if you can partition the array 
+into two subsets such that the sum of the elements in both subsets is equal 
+or false otherwise.
+*/
+
+// https://leetcode.com/problems/partition-equal-subset-sum/description/
+
+/*
+brute force: backtracking
+if sum % 2 !== 0, return false
+*/
+var canPartition = function (nums) {};
