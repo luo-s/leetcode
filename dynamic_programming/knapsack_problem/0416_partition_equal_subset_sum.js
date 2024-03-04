3c2809161010dfba4685670800cd498eb9c5f694
Shu <luo.s@outlook.com>
2024-03-04 14:27:57 -0500

    0-1 knapsack

--- dynamic_programming/knapsack_problem/0416_partition_equal_subset_sum.js
+++ dynamic_programming/knapsack_problem/0416_partition_equal_subset_sum.js
@@ -13,11 +13,14 @@ or false otherwise.
 /*
 brute force: backtracking get all subsets, check if any subset's sum is equal to sum / 2
 if sum % 2 !== 0, return false
+time complexity: O(2^n)
+space complexity: O(2^n * n)
 */
 var canPartition = function (nums) {
   let sum = nums.reduce((acc, cur) => acc + cur, 0);
   if (sum % 2) return false;
-  let target = sum / 2;
+  let target = nums.reduce((acc, cur) => acc + cur, 0) / 2;
+  // backtracking to get all subsets
   var subsetsWithDup = function (nums) {
     let result = [];
     let path = [];
@@ -41,4 +44,45 @@ var canPartition = function (nums) {
   return subsetsWithDup(nums).some(
     (subset) => subset.reduce((acc, cur) => acc + cur, 0) === target
   );
+};
+
+/* dynamic programming
+this is a 0/1 knapsack problem
+sum / 2 is the capacity of the knapsack
+let dp[i][w] be true if there is a subset of the first i elements that has a sum of w
+if w < nums[i - 1], dp[i][w] = dp[i - 1][w]
+else, dp[i][w] = dp[i - 1][w] || dp[i - 1][w - nums[i - 1]]
+time complexity: O(n * sum)
+space complexity: O(n * sum)
+*/
+var canPartition = function (nums) {
+  let sum = nums.reduce((acc, cur) => acc + cur, 0);
+  if (sum % 2) return false;
+  let target = sum / 2;
+  let n = nums.length;
+  let dp = new Array(n + 1)
+    .fill(0)
+    .map(() => new Array(target + 1).fill(false));
+  // dp[i][0] is always true: empty case -> sum == 0 == w
+  for (let i = 0; i <= n; i++) {
+    dp[i][0] = true;
+  }
+  // dp[0][w] is always false: empty subset -> sum == 0 < w
+  for (let w = 0; w <= target; w++) {
+    dp[0][w] = false;
+  }
+  // loop throuth all elements
+  for (let i = 1; i <= n; i++) {
+    // loop throuth all possible sum
+    for (let w = 1; w <= target; w++) {
+      // if nums[i-1] > limit, we can't include it
+      if (w < nums[i - 1]) {
+        dp[i][w] = dp[i - 1][w];
+        // else we can include or not include
+      } else {
+        dp[i][w] = dp[i - 1][w] || dp[i - 1][w - nums[i - 1]];
+      }
+    }
+  }
+  return dp[n][target];
 };
