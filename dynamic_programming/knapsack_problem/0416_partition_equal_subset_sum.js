9096b2b4cd5a140b182a9f864004a91d95b33fd0
Shu <luo.s@outlook.com>
2024-03-04 13:34:39 -0500

    brute force

--- dynamic_programming/knapsack_problem/0416_partition_equal_subset_sum.js
+++ dynamic_programming/knapsack_problem/0416_partition_equal_subset_sum.js
@@ -2,12 +2,43 @@
 Given an integer array nums, return true if you can partition the array 
 into two subsets such that the sum of the elements in both subsets is equal 
 or false otherwise.
+
+1 <= nums.length <= 200
+1 <= nums[i] <= 100
 */
 
 // https://leetcode.com/problems/partition-equal-subset-sum/description/
+// related https://leetcode.com/problems/subsets-ii/description/
 
 /*
-brute force: backtracking
+brute force: backtracking get all subsets, check if any subset's sum is equal to sum / 2
 if sum % 2 !== 0, return false
 */
-var canPartition = function (nums) {};
+var canPartition = function (nums) {
+  let sum = nums.reduce((acc, cur) => acc + cur, 0);
+  if (sum % 2) return false;
+  let target = sum / 2;
+  var subsetsWithDup = function (nums) {
+    let result = [];
+    let path = [];
+    nums.sort((a, b) => a - b);
+    let used = new Array(nums.length).fill(false);
+    var backtracking = function (nums, index) {
+      result.push([...path]);
+      if (index >= nums.length) return;
+      for (let i = index; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue;
+        path.push(nums[i]);
+        used[i] = true;
+        backtracking(nums, i + 1);
+        path.pop();
+        used[i] = false;
+      }
+    };
+    backtracking(nums, 0);
+    return result;
+  };
+  return subsetsWithDup(nums).some(
+    (subset) => subset.reduce((acc, cur) => acc + cur, 0) === target
+  );
+};
