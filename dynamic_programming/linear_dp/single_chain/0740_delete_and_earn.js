3e35005a1db3c21cb15276b5b54243241a95c82f
Shu <luo.s@outlook.com>
2024-03-04 10:14:19 -0500

    wip

---
+++ dynamic_programming/linear_dp/single_chain/0740_delete_and_earn.js
@@ -0,0 +1,14 @@
+/*
+You are given an integer array nums. You want to maximize the number of 
+points you get by performing the following operation any number of times:
+1) Pick any nums[i] and delete it to earn nums[i] points. 
+2) Afterwards, you must delete EVERY element equal to nums[i] - 1 and 
+EVERY element equal to nums[i] + 1.
+
+Return the maximum number of points you can earn by applying the above 
+operation some number of times.
+*/
+
+// https://leetcode.com/problems/delete-and-earn/description/
+
+var deleteAndEarn = function (nums) {};
