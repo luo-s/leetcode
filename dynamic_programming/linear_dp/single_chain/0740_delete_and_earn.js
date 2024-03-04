d55f6d22ae5976d3e1c54d8cc469266936577fe3
Shu <luo.s@outlook.com>
2024-03-04 10:31:59 -0500

    good copy

--- dynamic_programming/linear_dp/single_chain/0740_delete_and_earn.js
+++ dynamic_programming/linear_dp/single_chain/0740_delete_and_earn.js
@@ -7,8 +7,29 @@ EVERY element equal to nums[i] + 1.
 
 Return the maximum number of points you can earn by applying the above 
 operation some number of times.
+
+1 <= nums.length <= 2 * 10^4
+1 <= nums[i] <= 10^4
 */
 
 // https://leetcode.com/problems/delete-and-earn/description/
+// related https://leetcode.com/problems/house-robber-ii/description/
 
-var deleteAndEarn = function (nums) {};
+// constrcut a new array to store the sum of the same number, then the
+// same as house robber problem
+var deleteAndEarn = function (nums) {
+  let max = Math.max(...nums),
+    min = Math.min(...nums),
+    array = new Array(max - min + 1).fill(0);
+  for (let num of nums) {
+    array[num - min] += num;
+  }
+  if (array.length === 1) return array[0];
+  let dp = new Array(array.length).fill(0);
+  dp[0] = array[0];
+  dp[1] = Math.max(array[0], array[1]);
+  for (let i = 2; i < array.length; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + array[i]);
+  }
+  return dp[array.length - 1];
+};
