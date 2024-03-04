5dfd2507469b9f17edabf5c2a7955482d14b0e9a
Shu <luo.s@outlook.com>
2024-03-04 11:55:35 -0500

    good copy

--- dynamic_programming/linear_dp/single_chain/0213_house_robber_II.js
+++ dynamic_programming/linear_dp/single_chain/0213_house_robber_II.js
@@ -14,7 +14,25 @@ return the maximum amount of money you can rob tonight without alerting the poli
 // https://leetcode.com/problems/house-robber-ii/description/
 
 /*
-circular: cannot rob both the first and the last house
-
+circular: cannot rob both the first and the last house.
+1. rob the first house, then we cannot rob the last house. nums(0, n-1)
+2. rob the last house, then we cannot rob the first house. rob(1)
+3. return the maximum of the two cases.
+note: those two cases both includes don't rob the first and the last house.
 */
-var rob = function (nums) {};
+var rob = function (nums) {
+  if (nums.length === 1) return nums[0];
+  var robHelper = function (nums) {
+    let dp = new Array(nums.length).fill(0);
+    dp[0] = nums[0];
+    dp[1] = Math.max(nums[0], nums[1]);
+    for (let i = 2; i < nums.length; i++) {
+      dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+    }
+    return dp[nums.length - 1];
+  };
+  return Math.max(
+    robHelper(nums.slice(0, nums.length - 1)),
+    robHelper(nums.slice(1))
+  );
+};
