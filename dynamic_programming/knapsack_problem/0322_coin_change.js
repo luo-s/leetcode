3c43b93f2324f0dcced163740a86454b5ca3049f
Shu <luo.s@outlook.com>
2024-03-05 15:21:53 -0500

    good copy

--- dynamic_programming/knapsack_problem/0322_coin_change.js
+++ dynamic_programming/knapsack_problem/0322_coin_change.js
@@ -7,8 +7,25 @@ If that amount of money cannot be made up by any combination of the coins,
 return -1.
 
 You may assume that you have an infinite number of each kind of coin.
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 2^31 - 1
+0 <= amount <= 10^4
 */
 
 // https://leetcode.com/problems/coin-change/
 
-var coinChange = function (coins, amount) {};
+/*
+let dp[i] be the fewest number of coins that sum to w
+dp[i] = min(dp[i], dp[i - coins[i]] + 1)
+*/
+var coinChange = function (coins, amount) {
+  let dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 0; i < coins.length; i++) {
+    for (let w = coins[i]; w <= amount; w++) {
+      dp[w] = Math.min(dp[w], dp[w - coins[i]] + 1);
+    }
+  }
+  return dp[amount] === Infinity ? -1 : dp[amount];
+};
