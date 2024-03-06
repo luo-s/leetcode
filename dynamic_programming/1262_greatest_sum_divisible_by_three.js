4565c316596ab1f8096dd0f09127e597be26e754
Shu <luo.s@outlook.com>
2024-03-06 16:41:29 -0500

    dp

--- dynamic_programming/1262_greatest_sum_divisible_by_three.js
+++ dynamic_programming/1262_greatest_sum_divisible_by_three.js
@@ -27,3 +27,23 @@ var maxSumDivThree = function (nums) {
   dfs(0, 0);
   return maxSum;
 };
+
+/*
+dynamic programming
+let dp[i][j] be the maximum sum of first i elements that sum % 3 = j
+dp[i][j] = max(dp[i-1][j], dp[i-1][(3 + j - nums[i]) % 3] + nums[i])
+*/
+var maxSumDivThree = function (nums) {
+  let n = nums.length;
+  let dp = new Array(n + 1).fill(0).map(() => new Array(3).fill(-Infinity));
+  dp[0][0] = 0;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 0; j < 3; j++) {
+      dp[i][j] = Math.max(
+        dp[i - 1][j],
+        dp[i - 1][(3 + j - (nums[i - 1] % 3)) % 3] + nums[i - 1]
+      );
+    }
+  }
+  return dp[n][0];
+};
