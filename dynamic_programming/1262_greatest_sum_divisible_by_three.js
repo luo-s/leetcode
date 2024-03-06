60aa60c87ed272d037fea3dde59d0d529547ae6a
Shu <luo.s@outlook.com>
2024-03-06 11:38:38 -0500

    wip

---
+++ dynamic_programming/1262_greatest_sum_divisible_by_three.js
@@ -0,0 +1,13 @@
+/*
+Given an integer array nums, return the maximum possible sum of elements 
+of the array such that it is divisible by three.
+
+1 <= nums.length <= 4 * 10^4
+1 <= nums[i] <= 10^4
+*/
+
+// https://leetcode.com/problems/greatest-sum-divisible-by-three/description/
+
+var maxSumDivThree = function (nums) {};
+
+console.log(maxSumDivThree([3, 6, 5, 1, 8])); // 18
