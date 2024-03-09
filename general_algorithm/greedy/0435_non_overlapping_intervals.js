03f7cc4764ac2d7583b02f72a5b95ab757856f3d
Shu <luo.s@outlook.com>
2024-03-09 16:35:13 -0500

    good ocpy

--- general_algorithm/greedy/0435_non_overlapping_intervals.js
+++ general_algorithm/greedy/0435_non_overlapping_intervals.js
@@ -14,29 +14,44 @@ intervals[i].length == 2
 overlap, we want to remove the larger one.
 sort the intervals by the start, then loop through the intervals.
 */
+// time complexity: O(nlogn)
+// space complexity: O(1)
 var eraseOverlapIntervals = function (intervals) {
   // sort the intervals by the start
   intervals.sort((a, b) => a[0] - b[0]);
   let count = 0,
-    range = [];
+    range = intervals[0];
   // loop through the intervals
-  for (let [start, end] of intervals) {
-    // initialize range with the first interval
-    if (!range.length) {
-      range = [start, end];
-      continue;
-    }
+  for (let i = 1; i < intervals.length; i++) {
     // if the current interval overlaps, remove the larger interval
-    if (start < range[1]) {
+    if (intervals[i][0] < range[1]) {
       count++;
       // remove the larger interval
-      range[1] = Math.min(range[1], end);
+      range[1] = Math.min(range[1], intervals[i][1]);
     }
     // if the current interval does not overlap with last interval
     // update range to the current interval
     else {
-      range = [start, end];
+      range = intervals[i];
     }
+  }
+  return count;
+};
+
+// another approach
+// time complexity: O(nlogn)
+// sapce complexity: O(1)
+var eraseOverlapIntervals = function (intervals) {
+  // sort the intervals by the end
+  intervals.sort((a, b) => a[1] - b[1]);
+  let prev = intervals[0],
+    count = 0;
+  for (let i = 1; i < intervals.length; i++) {
+    // if the current interval overlaps, remove the larger interval
+    if (prev[1] > intervals[i][0]) {
+      count++;
+      prev[0] = Math.max(prev[0], intervals[i][0]);
+    } else prev = intervals[i];
   }
   return count;
 };
