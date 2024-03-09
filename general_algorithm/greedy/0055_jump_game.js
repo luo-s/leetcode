241862200f990d42c05776dde8982e4e1fdf87ee
Shu <luo.s@outlook.com>
2024-03-09 18:44:51 -0500

    dp - tle

--- general_algorithm/greedy/0055_jump_game.js
+++ general_algorithm/greedy/0055_jump_game.js
@@ -8,4 +8,20 @@ Return true if you can reach the last index, or false otherwise.
 
 // https://leetcode.com/problems/jump-game/description/
 
-var canJump = function (nums) {};
+/*
+dynamic programming  -- time limit exceeded
+let dp[i] be true if we can reach the i-th index
+dp[i] = dp[i] || dp[j] && nums[j] >= i - j for all j < i
+*/
+// time complexity: O(n^2)
+// space complexity: O(n)
+var canJump = function (nums) {
+  let dp = new Array(nums.length).fill(false);
+  dp[0] = true;
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      dp[i] = dp[i] || (dp[j] && nums[j] >= i - j);
+    }
+  }
+  return dp[nums.length - 1];
+};
