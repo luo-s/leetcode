1ee8ff01b20489dba5ca36cd8c2a7eab38e37e28
Shu <luo.s@outlook.com>
2024-03-09 18:01:25 -0500

    good copy

--- general_algorithm/greedy/0452_minimum_number_of_arrows_to_burst_balloons.js
+++ general_algorithm/greedy/0452_minimum_number_of_arrows_to_burst_balloons.js
@@ -14,8 +14,33 @@ A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
 Given the array points, return the minimum number of arrows that must be shot 
 to burst all balloons.
 
+1 <= points.length <= 10^5
+points[i].length == 2
+-2^31 <= xstart < xend <= 2^31 - 1
 */
 
 // https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/description/
 
-var findMinArrowShots = function (points) {};
+/*
+merge the overlapping intervals to smaller interval
+we need the number of minimum non-overlapping intervals
+*/
+var findMinArrowShots = function (points) {
+  // sort points by the xStart ASC
+  points.sort((a, b) => a[0] - b[0]);
+  let prev = points[0],
+    array = [];
+  for (let i = 1; i < points.length; i++) {
+    // if overlapping
+    if (points[i][0] <= prev[1]) {
+      prev[0] = Math.max(prev[0], points[i][0]);
+      prev[1] = Math.min(prev[1], points[i][1]);
+    } else {
+      // if not overlapping
+      array.push(prev);
+      prev = points[i];
+    }
+  }
+  array.push(prev);
+  return array.length;
+};
