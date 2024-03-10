7eaa81c236b94f0af89e9ea288fa6de857cf4995
Shu <luo.s@outlook.com>
2024-03-10 00:11:37 -0500

    wip

---
+++ general_algorithm/greedy/0045_jump_game_II.js
@@ -0,0 +1,15 @@
+/*
+You are given a 0-indexed array of integers nums of length n. 
+You are initially positioned at nums[0].
+
+nums[i] represents the maximum length of a forward jump from index i. 
+In other words, if you are at nums[i], you can jump to any nums[i + j] where:
+0 <= j <= nums[i] and i + j < n
+
+Return the minimum number of jumps to reach nums[n - 1]. 
+The test cases are generated such that you can reach nums[n - 1].
+*/
+
+// https://leetcode.com/problems/jump-game-ii/description/
+
+var jump = function (nums) {};
