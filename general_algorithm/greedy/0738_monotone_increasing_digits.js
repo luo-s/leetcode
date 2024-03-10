259edaeb3be0b070628acb7212bacd14b47442df
Shu <luo.s@outlook.com>
2024-03-10 18:38:55 -0400

    wip

---
+++ general_algorithm/greedy/0738_monotone_increasing_digits.js
@@ -0,0 +1,11 @@
+/*
+An integer has monotone increasing digits if and only if each pair of 
+adjacent digits x and y satisfy x <= y.
+
+Given an integer n, return the largest number that is less than or equal to 
+n with monotone increasing digits.
+*/
+
+// https://leetcode.com/problems/monotone-increasing-digits/description/
+
+var monotoneIncreasingDigits = function (n) {};
