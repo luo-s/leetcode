60f14e773a5a68e75cabff1c80118423058e9136
Shu <luo.s@outlook.com>
2024-03-10 00:16:33 -0500

    dp

--- general_algorithm/greedy/0045_jump_game_II.js
+++ general_algorithm/greedy/0045_jump_game_II.js
@@ -12,4 +12,23 @@ The test cases are generated such that you can reach nums[n - 1].
 
 // https://leetcode.com/problems/jump-game-ii/description/
 
-var jump = function (nums) {};
+/* dynamic programming
+let dp[i] be the minimum number of jumps to reach nums[i]
+dp[i] = Math.min(dp[i], dp[j] + 1) for all j < i and j + nums[j] >= i;
+if nums[i] cannot be reached, dp[i] = Infinity
+*/
+// time complexity: O(n^2)
+// space complexity: O(n)
+var jump = function (nums) {
+  let dp = new Array(nums.length).fill(Infinity);
+  // initializaiton: need 0 jumps to reach nums[0]
+  dp[0] = 0;
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (j + nums[j] >= i) {
+        dp[i] = Math.min(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  return dp[nums.length - 1];
+};
