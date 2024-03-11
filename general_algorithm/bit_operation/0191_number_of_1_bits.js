dae4be5a64dbd3618f1abe5b1a87a0a225c2fa09
Shu <luo.s@outlook.com>
2024-03-11 09:19:18 -0400

    good copy

--- general_algorithm/bit_operation/0191_number_of_1_bits.js
+++ general_algorithm/bit_operation/0191_number_of_1_bits.js
@@ -2,6 +2,8 @@
 Write a function that takes the binary representation of an unsigned 
 integer and returns the number of '1' bits it has 
 (also known as the Hamming weight).
+
+The input must be a binary string of length 32.
 */
 
 // one-liner
@@ -11,4 +13,14 @@ var hammingWeight = function (n) {
     .split("")
     .map(Number)
     .reduce((a, c) => a + c);
+};
+
+var hammingWeight = function (n) {
+  let count = 0;
+  while (n) {
+    count += n & 1;
+    // n is an unsigned integer, so we can use >>> instead of >>
+    n >>>= 1;
+  }
+  return count;
 };
