de0a9f6116e9a253e4cc1fdbad6786276a95a27d
Shu <luo.s@outlook.com>
2024-03-11 16:01:17 -0400

    good copy

--- general_algorithm/bit_operation/0338_counting_bits.js
+++ general_algorithm/bit_operation/0338_counting_bits.js
@@ -5,6 +5,9 @@ Given an integer n, return an array ans of length n + 1 such that for each i
 
 // https://leetcode.com/problems/counting-bits/description/
 
+// brute force
+// time complexity: O(n * log(n))
+// space complexity: O(n)
 var countBits = function (n) {
   var numberOfBits = function (num) {
     let count = 0;
@@ -19,4 +22,21 @@ var countBits = function (n) {
     ans[i] = numberOfBits(i);
   }
   return ans;
+};
+
+/* dynamic programming
+let dp[i] be the number of 1's in the binary representation of i
+1) if i is odd, dp[i] = dp[i-1] + 1
+2) if i is even, dp[i] = dp[i/2] (least significant bit is 0, thus right 
+shift by one doesn't change the number of 1's)
+*/
+// time complexity: O(n)
+// space complexity: O(n)
+var countBits = function (n) {
+  let dp = new Array(n + 1).fill(0);
+  for (let i = 1; i < n + 1; i++) {
+    if (i % 2 === 1) dp[i] = dp[i - 1] + 1;
+    else dp[i] = dp[i >> 1];
+  }
+  return dp;
 };
