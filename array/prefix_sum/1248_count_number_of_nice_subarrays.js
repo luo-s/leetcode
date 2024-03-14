c9398be3efd5d858cf2d5529f3b4f011fb15bac0
Shu <luo.s@outlook.com>
2024-03-14 11:48:15 -0400

    good copy

--- array/prefix_sum/1248_count_number_of_nice_subarrays.js
+++ array/prefix_sum/1248_count_number_of_nice_subarrays.js
@@ -7,4 +7,14 @@ Return the number of nice sub-arrays.
 
 // https://leetcode.com/problems/count-number-of-nice-subarrays/
 
-var numberOfSubarrays = function (nums, k) {};
+var numberOfSubarrays = function (nums, k) {
+  let count = new Map(),
+    preOdd = 0,
+    ans = 0;
+  for (let i = 0; i < nums.length; i++) {
+    count.set(preOdd, (count.get(preOdd) || 0) + 1);
+    if (nums[i] % 2) preOdd += 1;
+    ans += count.get(preOdd - k) || 0;
+  }
+  return ans;
+};
