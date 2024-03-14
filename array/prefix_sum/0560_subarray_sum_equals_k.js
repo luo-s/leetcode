713fc90375da072314e151545164713a560c42cd
Shu <luo.s@outlook.com>
2024-03-14 11:49:35 -0400

    wip

---
+++ array/prefix_sum/0560_subarray_sum_equals_k.js
@@ -0,0 +1,10 @@
+/*
+Given an array of integers nums and an integer k, return the total number of 
+subarrays whose sum equals to k.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+*/
+
+// https://leetcode.com/problems/subarray-sum-equals-k/description/
+
+var numSubarraysWithSum = function (nums, goal) {};
