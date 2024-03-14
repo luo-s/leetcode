0e8f087326183ec11863c668ab9b85a864e043bb
Shu <luo.s@outlook.com>
2024-03-14 11:52:30 -0400

    wip

---
+++ array/prefix_sum/0974_subarray_sums_divisible_by_k.js
@@ -0,0 +1,11 @@
+/*
+Given an integer array nums and an integer k, return the number of non-empty 
+subarrays that have a sum divisible by k.
+
+A subarray is a contiguous part of an array.
+*/
+
+// https://leetcode.com/problems/subarray-sums-divisible-by-k/description/
+
+var subarraysDivByK = function (nums, k) {};
+s;
