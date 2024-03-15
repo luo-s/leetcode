32a15b32dcfb633c11046f9c8e34284c50b22fe6
Shu <luo.s@outlook.com>
2024-03-15 10:53:17 -0400

    good copy

--- linked_list/0328_odd_even_linked_list.js
+++ linked_list/0328_odd_even_linked_list.js
@@ -8,8 +8,28 @@ Note that the relative order inside both the even and odd groups should
 remain as it was in the input.
 
 You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+The number of nodes in the linked list is in the range [0, 10^4].
+-10^6 <= Node.val <= 10^6
 */
 
 // https://leetcode.com/problems/odd-even-linked-list/description/
 
-var oddEvenList = function (head) {};
+var oddEvenList = function (head) {
+  if (!head) return head;
+  let curr = head,
+    evenHead = head.next;
+  let index = 1;
+  while (curr.next && curr.next.next) {
+    let next = curr.next;
+    curr.next = next.next;
+    curr = next;
+    index++;
+  }
+  if (index % 2) {
+    curr.next = evenHead;
+  } else {
+    curr.next.next = evenHead;
+    curr.next = null;
+  }
+  return head;
+};
