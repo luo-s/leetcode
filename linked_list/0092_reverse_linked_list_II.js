9da21e14726ae1ef8f3d35d1c9a2e2083ccefeaf
Shu <luo.s@outlook.com>
2024-03-15 09:51:27 -0400

    cut, reverse, and connect solution

--- linked_list/0092_reverse_linked_list_II.js
+++ linked_list/0092_reverse_linked_list_II.js
@@ -6,19 +6,41 @@ right, and return the reversed list.
 // https://leetcode.com/problems/reverse-linked-list-ii/
 // https://leetcode.cn/problems/reverse-linked-list-ii/solution/fan-zhuan-lian-biao-ii-by-leetcode-solut-teyq/
 
+// cut, reverse, and connect
 var reverseBetween = function (head, left, right) {
-  const dummyNode = new ListNode(-1, head);
-  let pre = dummyNode;
-  for (let i = 0; i < left - 1; ++i) {
-    pre = pre.next;
+  var reverseList = function (node) {
+    let prev = new ListNode(0, node);
+    curr = node;
+    while (curr) {
+      const next = curr.next;
+      curr.next = prev;
+      prev = curr;
+      curr = next;
+    }
+    return prev;
+  };
+  const dummyNode = new ListNode(0, head);
+  let curr = dummyNode;
+  // move left - 1 steps
+  for (let i = 0; i < left - 1; i++) {
+    curr = curr.next;
   }
-
-  let cur = pre.next;
-  for (let i = 0; i < right - left; ++i) {
-    const next = cur.next;
-    cur.next = next.next;
-    next.next = pre.next;
-    pre.next = next;
+  let prevLeft = curr,
+    leftNode = curr.next;
+  // move another right - (left - 1) steps; move total right steps
+  for (let i = 0; i < right - left + 1; i++) {
+    curr = curr.next;
   }
+  let rightNode = curr;
+  let nextRight = curr.next;
+  // cut the list
+  prevLeft.next = null;
+  rightNode.next = null;
+  // reverse between leftNode and rightNode
+  reverseList(leftNode);
+  // connect the list
+  prevLeft.next = rightNode;
+  leftNode.next = nextRight;
+  // return head;
   return dummyNode.next;
 };
