3588945e88c4a35f293f4e32bd653b01bb779cb0
Shu <luo.s@outlook.com>
2024-03-15 02:09:15 -0400

    recursion

--- linked_list/0203_remove_linked_list_elements.js
+++ linked_list/0203_remove_linked_list_elements.js
@@ -3,6 +3,8 @@ Given the head of a linked list and an integer val, remove all the nodes of the
 linked list that has Node.val == val, and return the new head.
 */
 
+// https://leetcode.com/problems/remove-linked-list-elements/description/
+
 // time complexity O(n)
 // space complexity O(1)
 var removeElements = function (head, val) {
