101cbc20b78c5d7bb443052d17abc766af9779c6
Shu <luo.s@outlook.com>
2024-03-15 02:06:01 -0400

    destructuring assignment

--- linked_list/0206_reverse_linked_list.js
+++ linked_list/0206_reverse_linked_list.js
@@ -3,24 +3,14 @@ Given the head of a singly linked list, reverse the list, and return the
 reversed list.
 */
 
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-
 //
+
 var reverseList = function (head) {
   let prev = null;
   let curr = head;
   while (curr) {
     const next = curr.next;
+    // [curr.next, prev, curr] = [prev, curr, next];
     curr.next = prev;
     prev = curr;
     curr = next;
