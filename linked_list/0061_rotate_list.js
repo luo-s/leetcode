fa01811acb61049d945ecd5985bb62b7be0b8fca
Shu <luo.s@outlook.com>
2024-03-15 02:27:44 -0400

    wip

---
+++ linked_list/0061_rotate_list.js
@@ -0,0 +1,7 @@
+/*
+Given the head of a linked list, rotate the list to the right by k places.
+*/
+
+// https://leetcode.com/problems/rotate-list/description/
+
+var rotateRight = function (head, k) {};
