4a1bad5d09899e0ebd9ff8d492a84e2b470b7b73
Shu <luo.s@outlook.com>
2024-03-15 12:43:09 -0400

    wip

---
+++ linked_list/0019_remove_nth_node_from_end_of_list.js
@@ -0,0 +1,8 @@
+/*
+Given the head of a linked list, 
+remove the nth node from the end of the list and return its head.
+*/
+
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+
+var removeNthFromEnd = function (head, n) {};
