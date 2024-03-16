cada04d36aecb219213d170b135baacfc1edb0d4
Shu <luo.s@outlook.com>
2024-03-16 15:59:53 -0400

    wip

--- linked_list/0160_intersection_of_two_linked_lists.js
+++ linked_list/0160_intersection_of_two_linked_lists.js
@@ -5,4 +5,19 @@ If the two linked lists have no intersection at all, return null.
 
 // https://leetcode.com/problems/intersection-of-two-linked-lists/description/
 
-var getIntersectionNode = function (headA, headB) {};
+// brute force
+// time complexity: O(n * m)
+// space complexity: O(1)
+var getIntersectionNode = function (headA, headB) {
+  let currA = headA,
+    currB = headB;
+  while (currA) {
+    while (currB) {
+      if (currA === currB) return currA;
+      currB = currB.next;
+    }
+    currA = currA.next;
+    currB = headB;
+  }
+  return null;
+};
