450997bd8ca247812e4df8d16f9b453de6975960
Shu <luo.s@outlook.com>
2024-03-16 16:06:01 -0400

    good copy

--- linked_list/0876_middle_of_the_linked_list.js
+++ linked_list/0876_middle_of_the_linked_list.js
@@ -6,4 +6,12 @@ If there are two middle nodes, return the second middle node.
 
 // https://leetcode.com/problems/middle-of-the-linked-list/
 
-var middleNode = function (head) {};
+var middleNode = function (head) {
+  let slow = head,
+    fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+  }
+  return slow;
+};
