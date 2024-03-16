e7a6242da442e3aa72f1012bad7f8c636399975b
Shu <luo.s@outlook.com>
2024-03-16 16:22:15 -0400

    recursion

--- linked_list/0021_merge_two_sorted_lists.js
+++ linked_list/0021_merge_two_sorted_lists.js
@@ -9,4 +9,17 @@ Return the head of the merged linked list.
 
 // https://leetcode.com/problems/merge-two-sorted-lists/description/
 
-var mergeTwoLists = function (list1, list2) {};
+// recursion
+var mergeTwoLists = function (list1, list2) {
+  let head1 = list1,
+    head2 = list2;
+  if (!head1) return head2;
+  if (!head2) return head1;
+  if (head1.val < head2.val) {
+    head1.next = mergeTwoLists(head1.next, head2);
+    return head1;
+  } else {
+    head2.next = mergeTwoLists(head1, head2.next);
+    return head2;
+  }
+};
