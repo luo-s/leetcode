21d6086086f27f2a54139d71a55fa49fa2ad7659
Shu <luo.s@outlook.com>
2024-03-16 08:25:28 -0400

    wip

--- linked_list/0141_linked_list_cycle.js
+++ linked_list/0141_linked_list_cycle.js
@@ -1,58 +1,15 @@
 /*
-Given head, the head of a linked list, determine if the linked list has a cycle 
-in it.
+Given head, the head of a linked list, determine if the linked list has a 
+cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that 
+can be reached again by continuously following the next pointer. 
+Internally, pos is used to denote the index of the node that tail's next 
+pointer is connected to. Note that pos is not passed as a parameter.
 
 Return true if there is a cycle in the linked list. Otherwise, return false.
 */
+
 // https://leetcode.com/problems/linked-list-cycle/description/
 
-// Map fueatures
-// time complexity O(n)
-// space complexity O(n)
-var hasCycle = function (head) {
-  if (!head) return false;
-  const seen = new Map();
-  let curr = head;
-  while (curr.next) {
-    if (seen.has(curr)) {
-      return true;
-    } else {
-      seen.set(curr, true);
-    }
-    curr = curr.next;
-  }
-  return false;
-};
-
-// set fueatures
-// time complexity O(n)
-// space complexity O(n)
-var hasCycle = function (head) {
-  if (!head) return false;
-  const seen = new Set();
-  let curr = head;
-  while (curr.next) {
-    if (seen.has(curr)) {
-      return true;
-    } else {
-      seen.add(curr);
-    }
-    curr = curr.next;
-  }
-  return false;
-};
-
-// two pointers
-// time complexity O(n)
-// space complexity O(1)
-var hasCycle = function (head) {
-  if (!head) return false;
-  let fast = head;
-  let slow = head;
-  while (fast && fast.next) {
-    slow = slow.next;
-    fast = fast.next.next;
-    if (fast === slow) return true;
-  }
-  return false;
-};
+var hasCycle = function (head) {};
---
+++ linked_list/0160_intersection_of_two_linked_lists.js
@@ -0,0 +1,8 @@
+/*
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. 
+If the two linked lists have no intersection at all, return null.
+*/
+
+// https://leetcode.com/problems/intersection-of-two-linked-lists/description/
+
+var getIntersectionNode = function (headA, headB) {};
