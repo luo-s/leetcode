aae65532549195533eb12c474df1fa4dc4c5faba
Shu <luo.s@outlook.com>
2024-03-17 22:23:28 -0400

    wip

---
+++ stack/monotone_stack/0901_online_stock_span.js
@@ -0,0 +1,14 @@
+/*
+Design an algorithm that collects daily price quotes for some stock and 
+returns the span of that stock's price for the current day.
+
+The span of the stock's price in one day is the maximum number of consecutive 
+days (starting from that day and going backward) for which the stock price 
+was less than or equal to the price of that day.
+*/
+
+// https://leetcode.com/problems/online-stock-span/
+
+var StockSpanner = function () {};
+
+StockSpanner.prototype.next = function (price) {};
