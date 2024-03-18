2f0a6f057ce0ea234a4daa33de8123a089fd7c1f
Shu <luo.s@outlook.com>
2024-03-18 11:40:27 -0400

    wip

---
+++ stack/monotone_stack/0084_largest_rectangle_in_histogram.js
@@ -0,0 +1,9 @@
+/*
+Given an array of integers heights representing the histogram's bar height 
+where the width of each bar is 1, return the area of the largest rectangle 
+in the histogram.
+*/
+
+// https://leetcode.com/problems/largest-rectangle-in-histogram/description/
+
+var largestRectangleArea = function (heights) {};
