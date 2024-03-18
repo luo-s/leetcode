e14b8d3b09389eebeaebd7051b981f1283b3b516
Shu <luo.s@outlook.com>
2024-03-18 13:36:48 -0400

    add different versions of binary search

--- array/binary_search/0704_binary_search.js
+++ array/binary_search/0704_binary_search.js
@@ -14,18 +14,68 @@ You must write an algorithm with O(log n) runtime complexity.
 var search = function (nums, target) {
   let left = 0;
   let right = nums.length - 1;
+  // search [left, right]
   while (left <= right) {
+    // stop condition: left > right
     let mid = Math.floor((left + right) / 2);
     if (nums[mid] > target) {
+      // search [left, mid - 1]
       right = mid - 1;
     } else if (nums[mid] < target) {
+      // search [mid + 1, right]
       left = mid + 1;
     } else {
+      // found it
       return mid;
     }
   }
   return -1;
 };
 
-console.log(search([-1, 0, 3, 5, 9, 12], 9)); //  4
-console.log(search([-1, 0, 3, 5, 9, 12], 2)); // -1
+/* another version
+mid = Math.floor((left + right) / 2);
+nums[mid] < target: left = mid + 1;
+else: right = mid;
+*/
+var search = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+  // search [left, right]
+  while (left < right) {
+    // stop condition: left === right
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] < target) {
+      // exclude [left, mid], search [mid + 1, rihgt]
+      left = mid + 1;
+    } else if (nums[mid] >= target) {
+      // exclude [mid + 1, right], search [left, mid]
+      right = mid;
+    }
+  }
+  // final search: [left, left]
+  return nums[left] === target ? left : -1;
+};
+
+/* another version
+mid = Math.floor((left + right + 1) / 2);
+nums[mid] > target: right = mid - 1;
+else: left = mid;
+*/
+var search = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1;
+  // search [left, right]
+  while (left < right) {
+    // stop condition: left === right
+    let mid = Math.floor((left + right + 1) / 2);
+    if (nums[mid] > target) {
+      // exclude [mid, right], search [left, mid - 1]
+      right = mid - 1;
+    } else if (nums[mid] <= target) {
+      // exclude [left, mid - 1], search [mid, right]
+      left = mid;
+    }
+  }
+  // final search: [left, left]
+  return nums[left] === target ? left : -1;
+};
