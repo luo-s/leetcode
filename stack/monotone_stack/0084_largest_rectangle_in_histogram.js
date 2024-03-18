5f398e06a0e08d7be68dd8b86b70a7656ca6102a
Shu <luo.s@outlook.com>
2024-03-18 12:05:38 -0400

    good copy

--- stack/monotone_stack/0084_largest_rectangle_in_histogram.js
+++ stack/monotone_stack/0084_largest_rectangle_in_histogram.js
@@ -6,4 +6,47 @@ in the histogram.
 
 // https://leetcode.com/problems/largest-rectangle-in-histogram/description/
 
-var largestRectangleArea = function (heights) {};
+/*
+loop through the heights, calculate the max area based on the current height
+need to know the left and right boundary of the current height
+let left[i] be the number of columns that continuous >= heights[i] from the left
+let right[i] be the number of columns that continuous >= heights[i] from the right
+the current max are is heights[i] * (left[i] + right[i] + 1)
+*/
+var largestRectangleArea = function (heights) {
+  let left = new Array(heights.length).fill(0),
+    right = new Array(heights.length).fill(0);
+  let stack = [];
+  for (let i = 0; i < heights.length; i++) {
+    while (stack.length > 0 && heights[stack[stack.length - 1]] > heights[i]) {
+      let index = stack.pop();
+      right[index] = i - index - 1;
+    }
+    stack.push(i);
+    if (i === heights.length - 1) {
+      while (stack.length > 0) {
+        let index = stack.pop();
+        right[index] = i - index;
+      }
+    }
+  }
+  stack = [];
+  for (let i = heights.length - 1; i >= 0; i--) {
+    while (stack.length > 0 && heights[stack[stack.length - 1]] > heights[i]) {
+      let index = stack.pop();
+      left[index] = index - i - 1;
+    }
+    stack.push(i);
+    if (i === 0) {
+      while (stack.length > 0) {
+        let index = stack.pop();
+        left[index] = index - i;
+      }
+    }
+  }
+  let maxArea = 0;
+  for (let i = 0; i < heights.length; i++) {
+    maxArea = Math.max(maxArea, heights[i] * (left[i] + right[i] + 1));
+  }
+  return maxArea;
+};
