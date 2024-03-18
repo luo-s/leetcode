16afae153b5d2889788d1abea4e85ed138fc666c
Shu <luo.s@outlook.com>
2024-03-18 19:26:50 -0400

    good copy

--- array/binary_search/0153_find_minimum_in_rotated_sorted_array.js
+++ array/binary_search/0153_find_minimum_in_rotated_sorted_array.js
@@ -18,23 +18,20 @@ You must write an algorithm that runs in O(log n) time.
 */
 // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
 
-// better version
+// binary search
 var findMin = function (nums) {
-  let left = 0;
-  let right = nums.length - 1;
+  let left = 0,
+    right = nums.length - 1;
+  // search [0, nums.length - 1], ends when left === right
   while (left < right) {
-    const mid = left + Math.floor((right - left) / 2);
+    let mid = left + Math.floor((right - left) / 2);
     if (nums[mid] < nums[right]) {
+      // mid is in the right part
       right = mid;
-    } else {
+    } else if (nums[mid] > nums[right]) {
+      // mid is in the left part
       left = mid + 1;
     }
   }
   return nums[left];
 };
-
-console.log(findMin([3, 4, 5, 1, 2])); // 1
-console.log(findMin([4, 5, 6, 7, 0, 1, 2])); // 0
-console.log(findMin([11, 13, 15, 17, 9])); // 9
-console.log(findMin([1, 2])); // 1
-console.log(findMin([2, 1])); // 1
