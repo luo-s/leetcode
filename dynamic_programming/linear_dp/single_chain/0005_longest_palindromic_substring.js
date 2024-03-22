c063676a8fba3e3a0b2a6ff82a5de5c04ac68a91
Shu <luo.s@outlook.com>
2024-03-21 22:59:29 -0400

    wip

---
+++ dynamic_programming/linear_dp/single_chain/0005_longest_palindromic_substring.js
@@ -0,0 +1,7 @@
+/*
+Given a string s, return the longest palindromic substring in s.
+*/
+
+// https://leetcode.com/problems/longest-palindromic-substring/description/
+
+var longestPalindrome = function (s) {};
