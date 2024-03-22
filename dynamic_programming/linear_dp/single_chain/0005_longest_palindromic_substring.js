c20fba2016dc01b5c3627ce4e1e92b9965694344
Shu <luo.s@outlook.com>
2024-03-21 23:08:03 -0400

    dp

--- dynamic_programming/linear_dp/single_chain/0005_longest_palindromic_substring.js
+++ dynamic_programming/linear_dp/single_chain/0005_longest_palindromic_substring.js
@@ -4,4 +4,26 @@ Given a string s, return the longest palindromic substring in s.
 
 // https://leetcode.com/problems/longest-palindromic-substring/description/
 
-var longestPalindrome = function (s) {};
+/* dynamic programming
+let dp[i][j] be true if s[i:j] is a palindrome
+1) if s[i] === s[j]:
+  1. if j - i < 3, dp[i][j] = true
+  2. if j - i >= 3, dp[i][j] = dp[i + 1][j - 1]
+2) if s[i] !== s[j], dp[i][j] = false
+*/
+var longestPalindrome = function (s) {
+  if (s.length < 2) return s;
+  let dp = new Array(s.length)
+    .fill(0)
+    .map(() => new Array(s.length).fill(false));
+  let res = "";
+  for (let i = s.length - 1; i >= 0; i--) {
+    for (let j = i; j < s.length; j++) {
+      dp[i][j] = s[i] === s[j] && (j - i < 3 || dp[i + 1][j - 1]);
+      if (dp[i][j] && j - i + 1 > res.length) {
+        res = s.slice(i, j + 1);
+      }
+    }
+  }
+  return res;
+};
