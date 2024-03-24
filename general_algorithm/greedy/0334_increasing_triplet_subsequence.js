88c4d0186f38054d0a14a9fcf48b4ddd383d591d
Shu <luo.s@outlook.com>
2024-03-23 23:44:30 -0400

    wip

---
+++ general_algorithm/greedy/0334_increasing_triplet_subsequence.js
@@ -0,0 +1,9 @@
+/*
+Given an integer array nums, return true if there exists a triple of 
+indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. 
+If no such indices exists, return false.
+*/
+
+// https://leetcode.com/problems/increasing-triplet-subsequence/description/
+
+var increasingTriplet = function (nums) {};
