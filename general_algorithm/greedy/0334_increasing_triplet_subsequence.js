8b5f1f839dca089942233894c0c3d480122f6d05
Shu <luo.s@outlook.com>
2024-03-23 23:44:48 -0400

    dp

--- general_algorithm/greedy/0334_increasing_triplet_subsequence.js
+++ general_algorithm/greedy/0334_increasing_triplet_subsequence.js
@@ -6,4 +6,21 @@ If no such indices exists, return false.
 
 // https://leetcode.com/problems/increasing-triplet-subsequence/description/
 
-var increasingTriplet = function (nums) {};
+/* dynamic programming -- time limit exceeded
+let dp[i] be the longest increasing subsequence that ending at nums[i] 
+that is strictly increasing.
+dp[i] = max(dp[i], dp[j]+ 1) for all k < i and nums[j] < nums[i].
+*/
+// time complexity: O(n^2)
+// space complexity: O(n)
+var increasingTriplet = function (nums) {
+  let dp = new Array(nums.length).fill(1);
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[j] < nums[i]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  return Math.max(...dp) >= 3;
+};
