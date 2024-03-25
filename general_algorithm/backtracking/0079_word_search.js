0ccff9eb935689319baa8995b79b36bde77c4428
Shu <luo.s@outlook.com>
2024-03-24 21:13:55 -0400

    good copy

--- general_algorithm/backtracking/0079_word_search.js
+++ general_algorithm/backtracking/0079_word_search.js
@@ -9,4 +9,34 @@ The same letter cell may not be used more than once.
 
 // https://leetcode.com/problems/word-search/description/
 
-var exist = function (board, word) {};
+var exist = function (board, word) {
+  let m = board.length,
+    n = board[0].length;
+  var dfs = function (i, j, index, visited) {
+    if (
+      i < 0 ||
+      j < 0 ||
+      i >= m ||
+      j >= n ||
+      board[i][j] !== word[index] ||
+      visited[i][j]
+    )
+      return false;
+    if (index === word.length - 1) return true;
+    visited[i][j] = true;
+    let res =
+      dfs(i + 1, j, index + 1, visited) ||
+      dfs(i - 1, j, index + 1, visited) ||
+      dfs(i, j + 1, index + 1, visited) ||
+      dfs(i, j - 1, index + 1, visited);
+    visited[i][j] = false;
+    return res;
+  };
+  visited = new Array(m).fill(0).map(() => new Array(n).fill(false));
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (dfs(i, j, 0, visited)) return true;
+    }
+  }
+  return false;
+};
