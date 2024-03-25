387e69e329d60fad63447391f19db5017097c138
Shu <luo.s@outlook.com>
2024-03-24 21:13:20 -0400

    wip

---
+++ general_algorithm/backtracking/0079_word_search.js
@@ -0,0 +1,12 @@
+/*
+Given an m x n grid of characters board and a string word, return true if 
+word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, 
+where adjacent cells are horizontally or vertically neighboring. 
+The same letter cell may not be used more than once.
+*/
+
+// https://leetcode.com/problems/word-search/description/
+
+var exist = function (board, word) {};
