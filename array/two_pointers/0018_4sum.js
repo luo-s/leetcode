7cb8d95202692645a658ab3b8c7d24b41f42e35d
Shu <40323042+luo-s@users.noreply.github.com>
2024-06-18 11:43:05 -0400

    good copy

--- array/two_pointers/0018_4sum.js
+++ array/two_pointers/0018_4sum.js
@@ -32,3 +32,33 @@ var fourSum = function (nums, target) {
   }
   return res;
 };
+
+// two pointers
+var fourSum = function (nums, target) {
+  let res = [];
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+      // define two pointers
+      let left = j + 1;
+      let right = nums.length - 1;
+      while (left < right) {
+        let sum = nums[i] + nums[j] + nums[left] + nums[right];
+        if (sum === target) {
+          res.push([nums[i], nums[j], nums[left], nums[right]]);
+          while (left < right && nums[left] === nums[left + 1]) left++;
+          while (left < right && nums[right] === nums[right - 1]) right--;
+          left++;
+          right--;
+        } else if (sum < target) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+  }
+  return res;
+};
