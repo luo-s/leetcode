caf4d1c288c8d37a22926bc774facf637ccb9d18
Shu <40323042+luo-s@users.noreply.github.com>
2024-06-17 21:38:26 -0400

    wip

--- array/two_pointers/0018_4sum.js
+++ array/two_pointers/0018_4sum.js
@@ -9,29 +9,23 @@ You may return the answer in any order.
 */
 // https://leetcode.com/problems/4sum/
 
+// brute force
+// Time complexity: O(n^4)
+// Space complexity: O(n)
 var fourSum = function (nums, target) {
   let res = [];
-  let n = nums.length;
-  if (n < 4) return res;
   nums.sort((a, b) => a - b);
-  for (let i = 0; i < n - 1; i++) {
-    if (i > 0 && nums[i] == nums[i - 1]) continue;
-    for (let j = i + 1; j < n; j++) {
-      if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-      let left = j + 1;
-      let right = n - 1;
-      while (left < right) {
-        const sum = nums[i] + nums[j] + nums[left] + nums[right];
-        if (sum == target) {
-          res.push([nums[i], nums[j], nums[left], nums[right]]);
-          while (left < right && nums[left] == nums[left + 1]) left++;
-          while (left < right && nums[right] == nums[right - 1]) right--;
-          left++;
-          right--;
-        } else if (sum < target) {
-          left++;
-        } else {
-          right--;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
+      for (let k = j + 1; k < nums.length; k++) {
+        if (k > j + 1 && nums[k] === nums[k - 1]) continue;
+        for (let l = k + 1; l < nums.length; l++) {
+          if (l > k + 1 && nums[l] === nums[l - 1]) continue;
+          if (nums[i] + nums[j] + nums[k] + nums[l] === target) {
+            res.push([nums[i], nums[j], nums[k], nums[l]]);
+          }
         }
       }
     }
