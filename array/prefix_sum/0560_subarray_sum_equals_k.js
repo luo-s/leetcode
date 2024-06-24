9511e5af6c0bc3bc800a103f54cd262b4fadf3fc
Shu <40323042+luo-s@users.noreply.github.com>
2024-06-23 22:32:00 -0400

    good copy

--- array/prefix_sum/0560_subarray_sum_equals_k.js
+++ array/prefix_sum/0560_subarray_sum_equals_k.js
@@ -8,16 +8,19 @@ A subarray is a contiguous non-empty sequence of elements within an array.
 // https://leetcode.com/problems/subarray-sum-equals-k/description/
 // related https://leetcode.com/problems/binary-subarrays-with-sum/description/
 
-var numSubarraysWithSum = function (nums, goal) {
+// a n-length array has n*(n+1)/2 subarrays
+// brute force requires O(n^2) time complexity
+// use prefix sum map to reduce time complexity to O(n)
+var subarraySum = function (nums, goal) {
   let cnt = new Map(),
     preSum = 0,
     ans = 0;
   // loop through the array
-  for (const num of nums) {
-    // count the prefix sum
+  for (let i = 0; i < nums.length; i++) {
+    // count how many times the same preSum appears from nums[0] to nums[i]
     cnt.set(preSum, (cnt.get(preSum) || 0) + 1);
     // update the prefix sum
-    preSum += num;
+    preSum += nums[i];
     // cnt.get(preSum - goal) is the number of subarrays that satisfy the condition
     ans += cnt.get(preSum - goal) || 0;
   }
