a1d6908aa71a3bb47cbb92edb1ab52a2902870de
Shu Luo <luo.s@outlook.com>
2024-08-26 15:06:16 -0400

    good copy

--- dynamic_programming/basics/1137_tribonacci_number.py
+++ dynamic_programming/basics/1137_tribonacci_number.py
@@ -3,3 +3,15 @@
 # Given n, return the value of Tn.
 
 # https://leetcode.com/problems/n-th-tribonacci-number/
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        dp = list(0 for _ in range(n + 1))
+        dp[0], dp[1], dp[2] = 0, 1, 1
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
+        return dp[n]
