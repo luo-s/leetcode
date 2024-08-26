21e22ba2f5af59c37cfb6aca818e04888207c28e
Shu Luo <luo.s@outlook.com>
2024-08-26 15:03:55 -0400

    good copy

---
+++ dynamic_programming/basics/0509_fibonacci_number.py
@@ -0,0 +1,18 @@
+# The Fibonacci numbers, commonly denoted F(n) form a sequence, called the 
+# Fibonacci sequence, such that each number is the sum of the two preceding ones, 
+# starting from 0 and 1.
+# F(0) = 0, F(1) = 1
+# F(n) = F(n - 1) + F(n - 2), for n > 1.
+# Given n, calculate F(n).
+
+# https://leetcode.com/problems/fibonacci-number/description/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 0, 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
