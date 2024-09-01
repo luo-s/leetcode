e715088ab50e6acec112408848ff4b612539f4e1
Shu <40323042+luo-s@users.noreply.github.com>
2024-08-31 22:40:50 -0400

    good copy

--- array/sliding_window/0003_longest_substr_without_repeating_characters.py
+++ array/sliding_window/0003_longest_substr_without_repeating_characters.py
@@ -13,5 +13,24 @@ class Solution:
                 max_cnt = max(max_cnt, len(chars))
             else:
                 chars.remove(s[slow])
+                # only move 1, has a lot repeated check
                 slow += 1
+        return max_cnt
+
+# optimized
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left, right, max_cnt, l = 0, 0, 0, len(s)
+        window = dict()
+        while right < l:
+            if s[right] not in window:
+                window[s[right]] = 1
+            else:
+                window[s[right]] += 1
+            # keep moving left until window only has unique char
+            while window[s[right]] > 1:
+                window[s[left]] -= 1
+                left += 1
+            max_cnt = max(max_cnt, right - left + 1)
+            right += 1
         return max_cnt
