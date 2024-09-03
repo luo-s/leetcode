2b6f61dc3438da9b892380bb3ec550d03980cff2
Shu Luo <luo.s@outlook.com>
2024-09-03 17:02:11 -0400

    good copy

--- linked_list/two_pointers/0876_middle_of_the_linked_list.py
+++ linked_list/two_pointers/0876_middle_of_the_linked_list.py
@@ -4,10 +4,20 @@
 
 # https://leetcode.com/problems/middle-of-the-linked-list/description/
 
+# return 2nd mid 
 class Solution:
     def middleNode(self, head: Optional[listNode]) -> Optional[listNode]:
         fast = slow = head
         while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+
+# return 1st mid
+class Solution:
+    def middleNode(self, head: Optional[listNode]) -> Optional[listNode]:
+        fast = slow = head
+        while fast.next and fast.next.next:
             slow = slow.next
             fast = fast.next.next
         return slow
