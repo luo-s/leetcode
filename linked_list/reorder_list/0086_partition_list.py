5e16cc7a8bf6c966b91d158326680c2d13751ba5
Shu <40323042+luo-s@users.noreply.github.com>
2024-09-03 17:32:49 -0400

    good copy

---
+++ linked_list/reorder_list/0086_partition_list.py
@@ -0,0 +1,22 @@
+# Given the head of a linked list and a value x, partition it such that all nodes 
+# less than x come before nodes greater than or equal to x.
+
+# You should preserve the original relative order of the nodes in each of the two partitions.
+
+# https://leetcode.com/problems/partition-list/description/
+
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        sml_dummy, big_dummy = ListNode(0), ListNode(0)
+        sml, big = sml_dummy, big_dummy
+        while head:
+            if head.val < x:
+                sml.next = head
+                sml = sml.next
+            else:
+                big.next = head
+                big = big.next
+            head = head.next
+        sml.next = big_dummy.next
+        big.next = None
+        return sml_dummy.next
