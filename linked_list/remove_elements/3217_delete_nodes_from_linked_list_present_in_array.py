3ef0ac949723604dcd10e8f9307dbb6cbd05a0fe
Shu Luo <luo.s@outlook.com>
2024-09-04 08:58:50 -0400

    good copy

---
+++ linked_list/remove_elements/3217_delete_nodes_from_linked_list_present_in_array.py
@@ -0,0 +1,17 @@
+# You are given an array of integers nums and the head of a linked list. 
+# Return the head of the modified linked list after removing all nodes 
+# from the linked list that have a value that exists in nums.
+
+# https://leetcode.com/problems/delete-nodes-from-linked-list-present-in-array/description/
+
+# search in array: O(n) will result in TLE
+# construct a hash map that give us O(1) time search
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        nums_set = set(nums)
+        cur = dummy_head = ListNode(0, head)
+        while cur:
+            while cur.next and cur.next.val in nums_set:
+                cur.next = cur.next.next
+            cur = cur.next
+        return dummy_head.next
