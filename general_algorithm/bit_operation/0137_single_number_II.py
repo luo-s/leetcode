e4c18e0a683f1dfd6a6d38e1f12a2c214af1ee29
Shu Luo <luo.s@outlook.com>
2024-09-06 09:55:02 -0400

    wip

---
+++ general_algorithm/bit_operation/0137_single_number_II.py
@@ -0,0 +1,12 @@
+# Given an integer array nums where every element appears three times except for one, 
+# which appears exactly once. Find the single element and return it.
+
+# You must implement a solution with a linear runtime complexity and use only 
+# constant extra space.
+
+# 1 <= nums.length <= 3 * 10 ** 4
+# -2 ** 31 <= nums[i] <= 2 ** 31 - 1
+# Each element in nums appears exactly three times except for one element which 
+# appears once.
+
+# https://leetcode.com/problems/single-number-ii/
