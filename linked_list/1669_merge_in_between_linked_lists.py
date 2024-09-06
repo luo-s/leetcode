25ac7d9ebc9629c252fe648943b0c1205cfc627a
Shu Luo <luo.s@outlook.com>
2024-09-06 06:56:33 -0400

    good copy

--- linked_list/1669_merge_in_between_linked_lists.py
+++ linked_list/1669_merge_in_between_linked_lists.py
@@ -6,3 +6,19 @@
 
 class Solution:
     def mergeInBetween(self, list1: ListNode, a: int, b: int, list2: ListNode) -> ListNode:
+        # find the pre_a and nxt_b node
+        cur = list1
+        for _ in range(a - 1):
+            cur = cur.next
+        pre_a = cur
+        for _ in range(b - a + 2):
+            cur = cur.next
+        nxt_b = cur
+        # connect list2 and nxt_b
+        cur = list2
+        while cur.next:
+            cur = cur.next
+        cur.next = nxt_b
+        # connect list2 and pre_a
+        pre_a.next = list2
+        return list1
