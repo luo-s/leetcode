b08a5cd414d367dc2f83dd906770f20cc14a2ca6
Shu Luo <luo.s@outlook.com>
2024-09-10 07:21:52 -0400

    good copy

---
+++ stack/basics/0946_validate_stack_sequences.py
@@ -0,0 +1,35 @@
+# Given two integer arrays pushed and popped each with distinct values, 
+# return true if this could have been the result of a sequence of push 
+# and pop operations on an initially empty stack, or false otherwise.
+
+# https://leetcode.com/problems/validate-stack-sequences/description/
+
+      
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack, p2 = [], 0
+        for num in pushed:
+            stack.append(num)
+            while stack and stack[-1] == popped[p2]:
+                p2 += 1
+                stack.pop()
+        return not stack
+
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack, l, p1, p2 = [], len(pushed), 0, 0
+        while p1 <= l and p2 <= l:
+            try: 
+                if not stack:
+                        stack.append(pushed[p1])
+                        p1 += 1
+                if stack[-1] != popped[p2]:
+                        stack.append(pushed[p1])
+                        p1 += 1
+                else:
+                    stack.pop()
+                    p2 += 1  
+                    if p2 == l: return True
+            except:
+                return False
+
