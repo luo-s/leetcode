03bf82411ccd4c3f88ddd0fcfb0126a6a238ff83
Shu Luo <luo.s@outlook.com>
2024-09-11 08:08:04 -0400

    good copy

--- graph/grid/dfs/0130_surrounded_regions.py
+++ graph/grid/dfs/0130_surrounded_regions.py
@@ -14,13 +14,13 @@ class Solution:
         Do not return anything, modify board in-place instead.
         """
         m, n = len(board), len(board[0])
-        # Mark 'O's on the border and their connected 'O's
+        # Mark 'O's to '#' on the border and their connected 'O's
         for i in range(m):
             self.dfs(board, i, 0)
-            self.dfs(board, i, n-1)
+            self.dfs(board, i, n - 1)
         for j in range(n):
             self.dfs(board, 0, j)
-            self.dfs(board, m-1, j)
+            self.dfs(board, m - 1, j)
         # Flip 'O's to 'X's and restore '#'s to 'O's
         for i in range(m):
             for j in range(n):
@@ -34,7 +34,7 @@ class Solution:
         if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != 'O':
             return
         board[i][j] = '#'
-        self.dfs(board, i+1, j)
-        self.dfs(board, i-1, j)
-        self.dfs(board, i, j+1)
-        self.dfs(board, i, j-1)
+        self.dfs(board, i + 1, j)
+        self.dfs(board, i - 1, j)
+        self.dfs(board, i, j + 1)
+        self.dfs(board, i, j - 1)
