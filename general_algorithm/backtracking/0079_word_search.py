63119e0730583b177b3a6f43dfac399ad78ad246
Shu <40323042+luo-s@users.noreply.github.com>
2024-09-12 22:59:12 -0400

    good copy

---
+++ general_algorithm/backtracking/0079_word_search.py
@@ -0,0 +1,25 @@
+# Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+# The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+# https://leetcode.com/problems/word-search/description/
+
+# backtracking
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def dfs(i, j, k):
+            # base case
+            if not 0 <= i < len(board) or not 0 <= j < len(board[0]) or board[i][j] != word[k]: return False
+            if k == len(word) - 1: return True
+            # recursive case
+            board[i][j] = ''
+            res = dfs(i + 1, j, k + 1) or dfs(i - 1, j, k + 1) or dfs(i, j + 1, k + 1) or dfs(i, j - 1, k + 1)
+            board[i][j] = word[k]
+            return res
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if dfs(i, j, 0): return True
+        return False
+        
+
