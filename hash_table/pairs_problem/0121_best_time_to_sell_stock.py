14a84f215a7abf43d600f97878ef885ba36d57bd
Shu Luo <luo.s@outlook.com>
2024-09-17 15:33:59 -0400

    good copy

---
+++ hash_table/pairs_problem/0121_best_time_to_sell_stock.py
@@ -0,0 +1,30 @@
+# You are given an array prices where prices[i] is the price of a given stock
+#  on the ith day.
+
+# You want to maximize your profit by choosing a single day to buy one stock 
+# and choosing a different day in the future to sell that stock.
+
+# Return the maximum profit you can achieve from this transaction. If you 
+# cannot achieve any profit, return 0.
+
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# LC 122 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+# LC 123 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+# LC 184 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
+# LC 309 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/
+# LC 714 https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+# advanced topic: pairs problem
+# LC 2905 https://leetcode.com/problems/find-indices-with-index-and-value-difference-ii/
+# LC 2874 https://leetcode.com/problems/maximum-value-of-an-ordered-triplet-ii/
+
+# if prices[i + 1] <= price[i], never buy prices[i]
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        cur_min, max_profit = float('inf'), 0
+        for price in prices:
+            if price <= cur_min:
+                cur_min = price
+            else:
+                max_profit = max(max_profit, price - cur_min)
+        return max_profit
