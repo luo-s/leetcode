09d6d39c681cab70dc714e6f8110aa5e67670141
Shu Luo <luo.s@outlook.com>
2024-09-18 15:17:25 -0400

    good copy

--- dynamic_programming/memoization_search/0494_target_sum.py
+++ dynamic_programming/memoization_search/0494_target_sum.py
@@ -5,10 +5,10 @@
 # For example, if nums = [2, 1], you can add a '+' before 2 and a '-' before 1 and concatenate them to build the expression "+2-1".
 # Return the number of different expressions that you can build, which evaluates to target.
 
-# https://leetcode.com/problems/target-sum/description/
+# https://leetcode.com/problems/target-sum/
 
 # dfs
-# let number of different expression that start with position = i and sum = cur_sum, end with position = l, sum = target is dfs(i, cur_sum)
+# let dfs(i, cur_sum) denote the number of different expression that start with position = i and sum = cur_sum, end with position = l, sum = target 
 # dfs formular: dfs(i, curSum) = dfs(i + 1, cur_sum - nums[i]) + dfs(i + 1, cur_sum + nums[i])
 # time complexity O(2 ** n) -- TLE
 class Solution:
@@ -26,10 +26,13 @@ class Solution:
         l = len(nums)
         table = dict()
         def dfs(i, cur_sum):
-            if i == l:
-              return 1 if cur_sum == target else 0
+            # check memo first
             if (i, cur_sum) in table:
                 return table[(i, cur_sum)]
+            # base case
+            if i == l:
+              return 1 if cur_sum == target else 0
+            # recursive case
             cnt = dfs(i + 1, cur_sum - nums[i]) + dfs(i + 1, cur_sum + nums[i])
             table[(i, cur_sum)] = cnt
             return cnt
