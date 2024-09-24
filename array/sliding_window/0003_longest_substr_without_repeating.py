238c3ba3da7e27dcf30958bd3249675602aa246d
Shu Luo <luo.s@outlook.com>
2024-09-24 07:44:31 -0400

    good copy

--- array/sliding_window/0003_longest_substr_without_repeating.py
+++ array/sliding_window/0003_longest_substr_without_repeating.py
@@ -13,20 +13,15 @@ class Solution:
                 max_cnt = max(max_cnt, len(chars))
             else:
                 chars.remove(s[slow])
-                # only move 1, has a lot repeated check
                 slow += 1
         return max_cnt
 
-# optimized
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         left, right, max_cnt, l = 0, 0, 0, len(s)
         window = dict()
         while right < l:
-            if s[right] not in window:
-                window[s[right]] = 1
-            else:
-                window[s[right]] += 1
+            window[s[right]] = window.get(s[right], 0) + 1
             # keep moving left until window only has unique char
             while window[s[right]] > 1:
                 window[s[left]] -= 1
@@ -34,3 +29,17 @@ class Solution:
             max_cnt = max(max_cnt, right - left + 1)
             right += 1
         return max_cnt
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left, right, ans, l = 0, 0, 0, len(s)
+        char_map = dict()
+        while right < l:
+            char_map[s[right]] = char_map.get(s[right], 0) + 1
+            while char_map[s[right]] > 1:
+                char_map[s[left]] -= 1
+                if char_map[s[left]] == 0: del char_map[s[left]]
+                left += 1
+            ans = max(ans, len(char_map))
+            right += 1
+        return max(ans, len(char_map))
