9bbb8f03a0148837b44331e104edc936d2d6eb82
Shu <40323042+luo-s@users.noreply.github.com>
2024-09-26 14:39:45 -0400

    good copy

--- array/basics/0066_plus_one.py
+++ array/basics/0066_plus_one.py
@@ -1,18 +1,35 @@
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+# Increment the large integer by one and return the resulting array of digits.
+
+# https://leetcode.com/problems/plus-one/
+# similar 
+# LC 415 https://leetcode.com/problems/add-strings/
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        l = len(digits)
+        ptr = l - 1
+        carry = 1
+        while ptr >= 0 or carry > 0:
+            if ptr >= 0: 
+                carry += digits[ptr]
+                digits[ptr] = carry % 10
+                ptr -= 1
+            else:
+                digits.insert(0, carry)
+            carry //= 10
+        return digits
+
 class Solution:
     def plusOne(self, digits: list[int]) -> list[int]:
-        l, index = len(digits), -1
-        # find the first index that digits[index] != 9 from last
-        for i in range(l-1, -1, -1):
-            if digits[i] != 9:
-                index = i
-                break
-        # edge case: all 9s
-        if index == -1:
-            temp = ([0] * l)
-            temp.insert(0, 1)
-            return temp
-        # plus one
-        for i in range(l-1, i, -1):
-            digits[i] = 0
-        digits[index] += 1
+        l = len(digits)
+        ptr = l - 1
+        while ptr >= 0 and digits[ptr] == 9:
+            digits[ptr] = 0
+            ptr -= 1
+        if ptr < 0:
+            digits.insert(0, 1)
+        else:
+            digits[ptr] += 1
         return digits
