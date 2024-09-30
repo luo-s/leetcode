8edfe24e7e4470174e0a8938f23aaccc0e53a342
Shu Luo <luo.s@outlook.com>
2024-09-30 11:17:01 -0400

    good copy

---
+++ stack/basics/0853_car_fleet.py
@@ -0,0 +1,28 @@
+
+# There are n cars at given miles away from the starting mile 0, traveling to reach the mile target.
+
+# You are given two integer array position and speed, both of length n, where position[i] is the starting mile of the ith car and speed[i] is the speed of the ith car in miles per hour.
+
+# A car cannot pass another car, but it can catch up and then travel next to it at the speed of the slower car.
+
+# A car fleet is a car or cars driving next to each other. The speed of the car fleet is the minimum speed of any car in the fleet.
+
+# If a car catches up to a car fleet at the mile target, it will still be considered as part of the car fleet.
+
+# Return the number of car fleets that will arrive at the destination.
+
+# https://leetcode.com/problems/car-fleet/
+
+# stack and sort
+# time complexity O(nlogn)
+# space complexity O(n)
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        pair = [(p, s) for p, s in zip(position, speed)]
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
