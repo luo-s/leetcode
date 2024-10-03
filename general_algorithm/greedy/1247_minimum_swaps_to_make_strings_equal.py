a50f7863362a0e068bd575647e3ac234a4a5b51f
Shu <40323042+luo-s@users.noreply.github.com>
2024-10-02 23:57:52 -0400

    good copy

--- general_algorithm/greedy/1247_minimum_swaps_to_make_strings_equal.py
+++ general_algorithm/greedy/1247_minimum_swaps_to_make_strings_equal.py
@@ -5,7 +5,25 @@
 # https://leetcode.com/problems/minimum-swaps-to-make-strings-equal/
 
 # for all the differences d: if d is odd, will never make it even
-# for every pair of d:
+# for allthe differences: there are 2 situations: xy and yx
+# 2 ways of change: 
+#   1) swap same position: s1[i] = x and s2[i] = y: xy -= 1 and yx += 1
+#   2) swap diff position: s1[i] = x and s2[j] = y: xy -= 1 and yx -= 1
+# the goal is to make xy = yx = 0
+# alway try method 2 first, until there is no qualifying pairs
+class Solution:
+    def minimumSwap(self, s1: str, s2: str) -> int:
+        xy, yx = 0, 0
+        n = len(s1)
+        for a, b in zip(s1, s2):
+            if a == 'x' and b == 'y':
+                xy += 1
+            if a == 'y' and b == 'x':
+                yx += 1
+        if (xy + yx) % 2 == 1:
+            return -1
+        return xy // 2 + yx // 2 + xy % 2 + yx % 2
+
 from collections import Counter
 class Solution:
     def minimumSwap(self, s1: str, s2: str) -> int:
